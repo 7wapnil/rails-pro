7335e598ce0e1612e7518d999e767092f69562e1
Rahul <rmishra@gammastack.com>
2018-04-26 11:55:50 +0300

    Add Discipline validation rules

--- spec/models/discipline_spec.rb
+++ spec/models/discipline_spec.rb
@@ -1,2 +1,10 @@
 describe Discipline, type: :model do
+  it { is_expected.to(validate_presence_of(:name)) }
+  it { is_expected.to(validate_presence_of(:kind)) }
+
+  it do
+    is_expected.to(
+      validate_inclusion_of(:kind).in_array(described_class::KINDS.values)
+    )
+  end
 end
