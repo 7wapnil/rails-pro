63e124e7b1158d9234ad7c3d651694e6d8723e41
Rahul <rmishra@gammastack.com>
2018-05-08 15:38:06 +0300

    Update CSGO prime script to fit new structure

--- db/prime/csgo.rb
+++ db/prime/csgo.rb
@@ -43,28 +43,28 @@ class CsgoPrimer
   REASONABLE_MATCH_TIME = 3.hours.freeze
 
   def self.discipline
-    Discipline.find_or_create_by!(name: 'CS:GO') do |discipline|
-      discipline.kind = 'esports'
+    @discipline ||= Discipline.find_or_create_by!(name: 'CS:GO') do |discipline|
+      discipline.kind = :esports
     end
   end
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
   def self.create_event(attributes)
-    tournament = Event.tournament.order(Arel.sql('RANDOM()')).first
+    tournament = EventScope.tournament.order(Arel.sql('RANDOM()')).first
     teams = CSGO_TEAMS.sample(2)
     event_name = "#{teams.first} vs #{teams.last}"
 
     default_attributes = {
       discipline: discipline,
-      event: tournament,
       name: event_name,
       description: "#{tournament.name}: #{event_name}",
-      kind: Event::KINDS[:match],
       start_at: Time.zone.now.beginning_of_hour
     }
 
-    event = Event.create!(default_attributes.merge(attributes))
+    event = Event.new(default_attributes.merge(attributes))
+    event.event_scopes << tournament
+    event.save!
 
     market = event.markets.create!(name: 'Match Winner', priority: 1)
 
@@ -79,20 +79,20 @@ end
 
 puts 'Checking Discipline ...'
 
-CsgoPrimer.discipline
+discipline = CsgoPrimer.discipline
 
 puts 'Checking Tournaments ...'
 
 CsgoPrimer::CSGO_TOURNAMENTS.each do |name|
-  Event.find_or_create_by!(name: name) do |tournament|
-    tournament.discipline = CsgoPrimer.discipline
-    tournament.kind = Event::KINDS[:tournament]
+  EventScope.find_or_create_by!(name: name) do |tournament|
+    tournament.discipline = discipline
+    tournament.kind = :tournament
   end
 end
 
 puts 'Finishing past in-play Matches ...'
 
-Event.match.where(
+Event.where(
   'start_at < ? AND end_at = ?',
   CsgoPrimer::REASONABLE_MATCH_TIME.ago,
   nil
@@ -102,7 +102,7 @@ end
 
 puts 'Checking current in-play Matches ...'
 
-if Event.match.where(
+if Event.where(
   'start_at > ? AND end_at = ?',
   CsgoPrimer::REASONABLE_MATCH_TIME.ago,
   nil
