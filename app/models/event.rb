3f8d1c56172283895b05a5cd019f152769d5c426
Rahul <rmishra@gammastack.com>
2018-05-10 15:39:13 +0300

    Add some querying logic to Events
    - default scope
    - today scope
    - #in_play?

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  default_scope do
+    where('start_at > ?', Date.yesterday.end_of_day).order(:start_at)
+  end
+
   belongs_to :discipline
   has_many :markets
   has_many :scoped_events
@@ -6,5 +10,15 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
 
-  scope :in_play, -> { where('start_at < ? AND end_at IS NULL', Time.zone.now) }
+  def self.in_play
+    where('start_at < ? AND end_at IS NULL', Time.zone.now)
+  end
+
+  def self.today
+    where(start_at: [Date.today.beginning_of_day..Date.today.end_of_day])
+  end
+
+  def in_play?
+    start_at.past? && end_at.nil?
+  end
 end
