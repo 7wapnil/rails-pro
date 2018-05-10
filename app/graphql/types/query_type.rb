0339e3c9304a94a394ef5ce21aee5018f7d1ef7f
Rahul <rmishra@gammastack.com>
2018-05-10 15:40:59 +0300

    Update API events field
    Allow filtering by discipline, kind or event_scope

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,15 +3,38 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  field :match do
-    type Types::MatchType
-    description 'Get a match by id'
+  field :event do
+    type Types::EventType
+    description 'Get an event by id'
     argument :id, !types.ID
     resolve ->(_obj, args, _ctx) { Event.find(args['id']) }
   end
 
-  field :matches, types[Types::MatchType] do
-    description 'Get all matches'
-    resolve ->(_obj, _args, _ctx) { Event.match.in_play }
+  field :events, types[Types::EventType] do
+    description 'Get all events'
+
+    argument :scope,
+             types.String,
+             prepare: ->(scope, _ctx) {
+               error_msg =
+                 'Scope should be one of following values: (discipline, kind, event_scope)'
+
+               return scope if scope.in? %w[discipline kind event_scope]
+               return GraphQL::ExecutionError.new(error_msg)
+             }
+    argument :query, types.String
+
+    resolve ->(_obj, args, _ctx) {
+      case args[:scope]
+      when 'kind'
+        Event.where(discipline: Discipline.select(:id).send(args[:query]))
+      when 'discipline'
+        Discipline.select(:id).find_by(name: args[:query]).events
+      when 'event_scope'
+        EventScope.select(:id).find_by(name: args[:query]).events
+      else
+        Event.all
+      end
+    }
   end
 end
