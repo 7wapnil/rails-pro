a45b0ab3805d983945efeec6bfe5fa568790c673
Rahul <rmishra@gammastack.com>
2018-05-10 15:37:58 +0300

    Rename GraphQL MatchType to EventType

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -1,8 +1,9 @@
-Types::MatchType = GraphQL::ObjectType.define do
-  name 'Match'
+Types::EventType = GraphQL::ObjectType.define do
+  name 'Event'
 
   field :id, !types.ID
   field :name, !types.String
+  field :description, !types.String
   field :start_at, types.String
   field :end_at, types.String
   field :markets, types[Types::MarketType]
