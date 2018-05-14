9ac021fd7baafa488968af30ca47b219c52d701e
Rahul <rmishra@gammastack.com>
2018-05-14 13:49:26 +0300

    Render events on dashboard with simple list

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="app">
     <h1 class="display-4">{{ message }}</h1>
+    <ul>
+      <li v-for="event in events"> {{ event.name }} </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag';
+
+const EVENTS_QUERY = gql`{
+  events {
+    id
+    name
+  }
+}
+`
+
 export default {
-  data: function () {
+  data () {
     return {
       message: "Hello, World"
     }
+  },
+  apollo: {
+    events: EVENTS_QUERY
   }
 }
 </script>
