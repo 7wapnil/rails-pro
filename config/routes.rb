ee644b48b253bbc0a04090ed66aaec368b966283
Rahul <rmishra@gammastack.com>
2018-05-16 16:21:18 +0300

    Add customer sign up flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: {
+    registrations: 'customers/registrations'
+  }
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
