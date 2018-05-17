d7b4b0707b8e7a58aa078274a37fa2a21cc8f25e
Rahul <rmishra@gammastack.com>
2018-05-17 16:34:04 +0300

    Fix rubocop offenses

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,5 +1,5 @@
 class DatePickerInput < SimpleForm::Inputs::Base
-  def input(wrapper_options = nil)
+  def input(_wrapper_options = nil)
     @builder.text_field(
       attribute_name,
       input_html_options.merge(class: 'form-control form_date')
