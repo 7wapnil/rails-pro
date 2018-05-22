432f160c3d628ccb0352c79ccf9a512592607b67
Rahul <rmishra@gammastack.com>
2018-05-22 15:26:13 +0300

    Add custom sign in / out redirects for backoffice

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,5 +25,15 @@ module Users
     # def configure_sign_in_params
     #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
     # end
+
+    private
+
+    def after_sign_in_path_for(_resource_or_scope)
+      backoffice_root_path
+    end
+
+    def after_sign_out_path_for(_resource_or_scope)
+      new_user_session_path
+    end
   end
 end
