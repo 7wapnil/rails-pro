e52cdfbce88927794238b5f9e62f1d56b38d780d
Rahul <rmishra@gammastack.com>
2018-05-22 12:24:12 +0300

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,40 @@
-# This file should contain all the record creation needed to seed
-# the database with its default values.
-# The data can then be loaded with the rails db:seed command
-# (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies =
-#     Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users_payload = [
+  {
+    first_name: 'Tim',
+    last_name: 'Hurks',
+    email: 't.hurks@arcanebet.com'
+  },
+  {
+    first_name: 'Claude',
+    last_name: 'Du Toit',
+    email: 'claude@arcanebet.com'
+  },
+  {
+    first_name: 'Stanislav',
+    last_name: 'Gorski',
+    email: 'stanislav@arcanebet.com'
+  },
+  {
+    first_name: 'Igor',
+    last_name: 'Murujev',
+    email: 'igor@arcanebet.com'
+  },
+  {
+    first_name: 'Bux',
+    last_name: 'Syed',
+    email: 'bux@arcanebet.com'
+  },
+  {
+    first_name: 'Tanel',
+    last_name: 'Liinak',
+    email: 'tanel@arcanebet.com'
+  }
+]
+
+users_payload.each do |payload|
+  User.find_or_create_by!(email: payload[:email]) do |user|
+    user.first_name = payload[:first_name]
+    user.last_name = payload[:last_name]
+    user.password = ENV['SEED_USER_DEFAULT_PASSWORD']
+  end
+end
