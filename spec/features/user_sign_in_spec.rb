917518d8c589d77a4adfa0a7e8adeaacd6318fa4
Rahul <rmishra@gammastack.com>
2018-05-23 09:56:59 +0300

    Add user sign in / out flow scenarios

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,79 @@
+describe 'User Sign In', type: :feature do
+  let(:user) { create(:user) }
+
+  context 'Anonymous user' do
+    it 'Can not access backoffice dashboard' do
+      visit backoffice_root_path
+
+      expect(current_path).to eq new_user_session_path
+      expect(page).to have_content I18n.t('devise.failure.unauthenticated')
+    end
+  end
+
+  context 'Sign In form' do
+    before { visit new_user_session_path }
+
+    it 'Successfully signs in' do
+      within 'form#new_user' do
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+
+        find('input[name="commit"]').click
+      end
+
+      expect(current_path).to eq backoffice_root_path
+      expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    end
+
+    it 'Fails to sign in if email or password is invalid' do
+      within 'form#new_user' do
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: 'iamwrong'
+
+        find('input[name="commit"]').click
+      end
+
+      expect_authentication_failure
+    end
+
+    it 'Fails to sign in if email is blank' do
+      within 'form#new_user' do
+        fill_in 'user_password', with: user.password
+
+        find('input[name="commit"]').click
+      end
+
+      expect_authentication_failure
+    end
+
+    it 'Fails to sign in if password is blank' do
+      within 'form#new_user' do
+        fill_in 'user_email', with: user.email
+
+        find('input[name="commit"]').click
+      end
+
+      expect_authentication_failure
+    end
+
+    context 'Signed in' do
+      it 'Sees own full name in navbar right corner' do
+        login_as user, scope: :user
+
+        visit backoffice_root_path
+
+        within 'nav.navbar' do
+          expect(page).to have_content user.full_name
+        end
+      end
+    end
+  end
+end
+
+def expect_authentication_failure
+  expect(current_path).to eq new_user_session_path
+  expect(page).to have_content I18n.t(
+    'devise.failure.invalid',
+    authentication_keys: 'Email'
+  )
+end
---
+++ spec/features/user_sign_out_spec.rb
@@ -0,0 +1,15 @@
+describe 'User Sign Out', type: :feature do
+  it 'Successfully signs out' do
+    user = create(:user)
+    login_as user, scope: :user
+
+    visit backoffice_root_path
+
+    within 'nav.navbar' do
+      click_link I18n.t(:sign_out)
+    end
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content I18n.t('devise.sessions.signed_out')
+  end
+end
