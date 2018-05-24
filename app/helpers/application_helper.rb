0b034da4120e48dec17440b7093ac484eb96595e
Rahul <rmishra@gammastack.com>
2018-05-24 15:50:22 +0300

    Add card helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def card(header: nil, &block)
+    content_tag(:div, class: 'card') do
+      concat content_tag(:h5, class: 'card-header') { header } if header
+      concat content_tag(:div, class: 'card-body') { yield }
     end
   end
 end
