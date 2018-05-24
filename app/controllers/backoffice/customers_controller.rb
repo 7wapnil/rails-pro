15b1415ff8f0916772b87c02f776491596376d76
Rahul <rmishra@gammastack.com>
2018-05-24 15:50:43 +0300

    Implement basic customers controller actions

--- app/controllers/backoffice/customers_controller.rb
+++ app/controllers/backoffice/customers_controller.rb
@@ -1,9 +1,11 @@
 module Backoffice
   class CustomersController < BackofficeController
     def index
+      @customers = Customer.all
     end
 
     def show
+      @customer = Customer.find(params[:id])
     end
   end
 end
