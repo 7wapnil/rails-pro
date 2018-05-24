db0e5d7675de7a74e45a602c0ea005f027814696
Rahul <rmishra@gammastack.com>
2018-05-24 10:37:41 +0300

    Rename Address#address to #street_address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_120705) do
+ActiveRecord::Schema.define(version: 2018_05_24_072436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_120705) do
     t.integer "country"
     t.string "state"
     t.string "city"
-    t.string "address"
+    t.string "street_address"
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
