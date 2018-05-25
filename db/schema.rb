a425d0a103e2c0a4b2075dc63346f86372c7ba6c
Rahul <rmishra@gammastack.com>
2018-05-25 10:14:52 +0300

    Change Address#country column type

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_072436) do
+ActiveRecord::Schema.define(version: 2018_05_25_065649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
     t.bigint "customer_id"
-    t.integer "country"
+    t.string "country"
     t.string "state"
     t.string "city"
     t.string "street_address"
