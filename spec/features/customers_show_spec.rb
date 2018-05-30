3b90a7394f63a8fe49c712a68e3121b607091a86
Rahul <rmishra@gammastack.com>
2018-05-30 13:16:05 +0300

    Add Customers#show specs

---
+++ spec/features/customers_show_spec.rb
@@ -0,0 +1,69 @@
+require Rails.root.join('spec/features/shared_examples/protected')
+
+describe 'Customers#show' do
+  subject { create(:customer) }
+
+  it_behaves_like 'protected',
+                  protected_path_name: :backoffice_customer_path,
+                  instance: true
+
+  context 'page content' do
+    before do
+      login_as create(:user), scope: :user
+      visit backoffice_customer_path(subject)
+    end
+
+    it 'shows account information' do
+      expect_to_have_section 'account-information'
+    end
+
+    it 'shows personal information' do
+      expect_to_have_section 'personal-information'
+    end
+
+    it 'shows contact information' do
+      expect_to_have_section 'contact-information'
+    end
+
+    context 'notes' do
+      it 'displays customer notes section' do
+        expect_to_have_section 'customer-notes'
+      end
+
+      it 'displays new note form' do
+        within '.card.customer-notes' do
+          expect(page).to have_selector 'form#new_customer_note'
+        end
+      end
+
+      it 'creates customer note' do
+        note_content = Faker::Lorem.paragraph
+
+        within 'form#new_customer_note' do
+          fill_in :customer_note_content, with: note_content
+          click_submit
+        end
+
+        within '.card.customer-notes' do
+          expect(page).to have_content note_content
+        end
+      end
+
+      it 'fails to create empty customer note' do
+        within 'form#new_customer_note' do
+          click_submit
+        end
+
+        within '.container' do
+          expect(page).to have_content "#{I18n.t(:content)} #{I18n.t('errors.messages.blank')}"
+        end
+      end
+    end
+
+    def expect_to_have_section(section_class)
+      within '.container' do
+        expect(page).to have_selector ".card.#{section_class}"
+      end
+    end
+  end
+end
