b566e8b755a190eff23ff961e68e1c8e1c0a21e2
Varun <vsharma@gammastack.com>
2018-05-31 11:50:45 +0300

    Add customer labels seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,4 +41,39 @@ users_payload.each do |payload|
   end
 end
 
+puts 'Checking customer labels list ...'
+
+customer_labels = [
+  { name: 'Normal' },
+  { name: 'VIP' },
+  { name: 'Arber' },
+  { name: 'Bonus Hunter' },
+  { name: 'Sharp' },
+  { name: 'Gambler' },
+  { name: 'Staff' },
+  { name: 'Watchlist' },
+  { name: 'Sharp_CS' },
+  { name: 'Shap_LoL' },
+  { name: 'Sharp_Dota' },
+  { name: 'Sharp_Tennis' },
+  { name: 'Sharp_Soccer' },
+  { name: 'Sharp_Basketball' },
+  { name: 'Limited' },
+  { name: 'Monitored' },
+  { name: 'POTVIP' },
+  { name: 'Industry' },
+  { name: 'Test_User' },
+  { name: 'Test_Customer' },
+  { name: 'Live Abuser' },
+  { name: 'Female' },
+  { name: 'Affiliate' },
+  { name: 'Youtuber' }
+]
+
+customer_labels.each do |payload|
+  Label.find_or_create_by!(name: payload[:name]) do |label|
+    label.name = payload[:name]
+  end
+end
+
 puts 'Done!'
