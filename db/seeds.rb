5c0f2976d54f0d30fb166b5d50eaa678078abed5
Rahul <rmishra@gammastack.com>
2018-06-06 14:42:48 +0300

    Add currencies seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require Rails.root.join('db/seeds/users')
 require Rails.root.join('db/seeds/customer_labels')
+require Rails.root.join('db/seeds/currencies')
 
 puts 'Done!'
---
+++ db/seeds/currencies.rb
@@ -0,0 +1,16 @@
+puts 'Checking currencies ...'
+
+currency_mapping = [
+  { name: 'Euro', code: 'EUR', primary: true },
+  { name: 'Swedish Kronor', code: 'SEK' },
+  { name: 'BitCoin', code: 'BTC' },
+  { name: 'milliBitCoin', code: 'mBTC' },
+  { name: 'Ethereum', code: 'ETH' }
+]
+
+currency_mapping.each do |payload|
+  Currency.find_or_create_by(name: payload[:name]) do |currency|
+    currency.code = payload[:code]
+    currency.primary = payload[:primary] || false
+  end
+end
