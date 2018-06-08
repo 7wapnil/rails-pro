f5b667998c12138a5c0811a571fa3c08bbe8615b
Rahul <rmishra@gammastack.com>
2018-06-08 15:05:11 +0300

    Simulate customer activity with dev:prime

--- db/prime/entries.rb
+++ db/prime/entries.rb
@@ -1,6 +1,6 @@
-puts 'Creating Deposits ...'
-
 random = Random.new
+
+puts 'Creating Deposits ...'
 
 Customer
   .order(Arel.sql('RANDOM()'))
@@ -18,6 +18,29 @@ Customer
         kind: :deposit,
         currency_code: currency.code,
         customer_id: customer.id,
+        amount: random.rand(rule.min_amount..rule.max_amount).round(2).to_f
+      }
+    )
+
+    WalletEntry::Service.call(request)
+  end
+end
+
+puts 'Simulating Customer activity ...'
+
+Wallet.find_each batch_size: 50 do |wallet|
+  bet_rule = EntryCurrencyRule.find_by!(currency: wallet.currency, kind: :bet)
+  win_rule = EntryCurrencyRule.find_by!(currency: wallet.currency, kind: :win)
+
+  random.rand(2..10).times do
+    kind = %i[bet win].sample
+    rule = binding.local_variable_get "#{kind}_rule"
+
+    request = EntryRequest.create!(
+      payload: {
+        kind: rule.kind,
+        currency_code: wallet.currency_code,
+        customer_id: wallet.customer_id,
         amount: random.rand(rule.min_amount..rule.max_amount).round(2).to_f
       }
     )
