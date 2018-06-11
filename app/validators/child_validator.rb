c49f4cece3e50331ca85436dee9cdbbe2e8f7b8e
Rahul <rmishra@gammastack.com>
2018-06-11 11:53:36 +0300

    Remove obsolete ChildValidator

--- app/validators/child_validator.rb
+++
@@ -1,7 +0,0 @@
-class ChildValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return if value.nil? || value.valid?
-
-    record.errors[attribute] << value.errors.full_messages
-  end
-end
