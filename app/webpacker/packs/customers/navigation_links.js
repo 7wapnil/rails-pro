58a6c065f712707a0e41359949aa934616384f47
Rahul <rmishra@gammastack.com>
2018-06-15 15:59:16 +0300

    Fix navigation js initialization on first load

--- app/webpacker/packs/customers/navigation_links.js
+++ app/webpacker/packs/customers/navigation_links.js
@@ -1,7 +1,16 @@
-document.addEventListener('turbolinks:load', () => {
+function highlightCurrent() {
   const actionName = $('body').data('action')
 
   $('.list-group-item.list-group-item-action').removeClass('active')
   $(`.list-group-item.list-group-item-action[data-target-action='${actionName}']`)
     .addClass('active')
-})
+}
+
+let loaded = false
+
+if (!loaded) {
+  document.addEventListener('turbolinks:load', highlightCurrent)
+  highlightCurrent()
+  loaded = true
+}
+
