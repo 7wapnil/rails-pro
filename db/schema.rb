a6bfaab5e86d8ed99b52220b0be6715fd66ed6f9
Varun <vsharma@gammastack.com>
2018-06-28 15:48:37 +0300

    Make customer notes model paranoid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_113417) do
+ActiveRecord::Schema.define(version: 2018_06_28_121337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,9 @@ ActiveRecord::Schema.define(version: 2018_06_28_113417) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["customer_id"], name: "index_customer_notes_on_customer_id"
+    t.index ["deleted_at"], name: "index_customer_notes_on_deleted_at"
     t.index ["user_id"], name: "index_customer_notes_on_user_id"
   end
 
