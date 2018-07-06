b7ee9f6f5a66a95a68f8e741bf0afc06017d6bdf
Rahul <rmishra@gammastack.com>
2018-07-06 17:02:40 +0300

    Fix Rails ORM-related generators after adding Mongoid

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Arcanebet
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.generators do |g|
+      g.orm :active_record
+    end
+
     # Settings in config/environments/*
     # take precedence over those specified here.
     # Application configuration can go into files in config/initializers
