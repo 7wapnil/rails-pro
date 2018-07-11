798b329b20b4e91fd9fedfc68156a8cbf009051a
Rahul <rmishra@gammastack.com>
2018-07-11 16:29:56 +0300

    Add bonuses#show

--- app/controllers/backoffice/bonuses_controller.rb
+++ app/controllers/backoffice/bonuses_controller.rb
@@ -4,5 +4,9 @@ module Backoffice
       @search = Bonus.search(query_params)
       @bonuses = @search.result.page(params[:page])
     end
+
+    def show
+      @bonus = Bonus.find(params[:id])
+    end
   end
 end
