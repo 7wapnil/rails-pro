aeb183d533b3fef7fc6117c5962bb1dab3b56fe5
Varun <vsharma@gammastack.com>
2018-07-17 12:27:57 +0300

    Basic Radar client unit tests

--- app/services/radar/client.rb
+++ app/services/radar/client.rb
@@ -3,14 +3,19 @@ module Radar
     include HTTParty
 
     base_uri ENV['RADAR_API_URL']
+    format :xml
 
     def initialize
       @options = { headers: { "x-access-token": ENV['RADAR_API_TOKEN'],
-                              "content-type": "application/xml" } }
+                              "content-type": 'application/xml' } }
     end
 
     def who_am_i
-      self.class.get('/users/whoami.xml', @options)
+      get_request('/users/whoami.xml')
+    end
+
+    def get_request(path)
+      self.class.get(path, @options)
     end
   end
 end
