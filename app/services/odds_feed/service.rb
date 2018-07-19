3e567299170032a2e02e62806dd11459f6e11ce9
Varun <vsharma@gammastack.com>
2018-07-19 11:40:13 +0300

    Update odds feed service with plan comment

--- app/services/odds_feed/service.rb
+++ app/services/odds_feed/service.rb
@@ -2,9 +2,15 @@ module OddsFeed
   class Service < ApplicationService
     def initialize(event_data)
       @event_data = event_data
+      @api = Radar::Client.new
     end
 
     def call
+      # store event
+      # store markets
+      # store odds
+      # add new odd values
+      # send updates to websocket
     end
   end
 end
