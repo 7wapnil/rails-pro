2d438c3ea328c4da910de57db97a9af4ed9ee1eb
Varun <vsharma@gammastack.com>
2018-07-23 16:57:26 +0300

    Use odds feed service in event processing worker

--- app/workers/event_processing_worker.rb
+++ app/workers/event_processing_worker.rb
@@ -2,6 +2,9 @@ class EventProcessingWorker
   include Sidekiq::Worker
 
   def perform(payload)
-    payload
+    Rails.logger.debug "Received job: #{payload}"
+    OddsFeed::Service.call(OddsFeed::Radar::Client.new, payload)
+  rescue => ex
+    Rails.logger.error ex.message
   end
 end
