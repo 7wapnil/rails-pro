e9ec6e843ed9271d3f4c4887a87b8255d2d1ca60
Mahendra <mtyagi@gammastack.com>
2018-07-24 11:42:58 +0300

    Dropping odds_values table

--- db/migrate/20180425071815_create_odd_values.rb
+++ db/migrate/20180425071815_create_odd_values.rb
@@ -1,10 +1,14 @@
 class CreateOddValues < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :odd_values do |t|
       t.references :odd, foreign_key: true
       t.decimal :value
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :odd_values
   end
 end
