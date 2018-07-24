27f278bae0ee296fd4cccbf4b9b3d76d849169df
Varun <vsharma@gammastack.com>
2018-07-24 11:17:34 +0300

    Add value validator in odd model

--- app/models/odd.rb
+++ app/models/odd.rb
@@ -3,4 +3,5 @@ class Odd < ApplicationRecord
   has_many :odd_values
 
   validates :name, :value, presence: true
+  validates :value, numericality: { greater_than: 0 }
 end
--- spec/models/odd_spec.rb
+++ spec/models/odd_spec.rb
@@ -4,4 +4,5 @@ describe Odd do
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:value) }
+  it { should validate_numericality_of(:value).is_greater_than(0) }
 end
