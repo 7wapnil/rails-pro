9ff606a744a8b193834ea88d3b5c419fdda3422a
Rahul <rmishra@gammastack.com>
2018-07-24 16:29:48 +0300

    Layout RadarMqProcessingWorker requirements for  current state

---
+++ spec/workers/radar_mq_processing_worker_spec.rb
@@ -0,0 +1,19 @@
+describe RadarMqProcessingWorker do
+  
+  describe '#perform' do
+    xit 'passes odds_change to EventProcessingWorker'
+    xit 'passes fixtures_fixture to EventProcessingWorker'
+    xit 'raises NotImplementedError on any unknown input'
+  end
+  
+  describe '.is_xml?' do
+    xit 'accepts one argument as input'
+    xit 'returns true for xml input'
+    xit 'returns false for non-xml input'
+  end
+  
+  describe '.root_element_name' do
+    xit 'accepts one argument as input'
+    xit 'returns xml root element name'
+  end
+end
