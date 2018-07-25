456a642067ff1681f8dc8a0d20ecd8cdce6a950b
Rahul <rmishra@gammastack.com>
2018-07-25 08:48:01 +0300

    Fix naming

--- spec/workers/radar_mq_processing_worker_spec.rb
+++ spec/workers/radar_mq_processing_worker_spec.rb
@@ -1,6 +1,6 @@
 describe RadarMqProcessingWorker do
   
-  describe '#perform' do
+  describe '.perform' do
     xit 'passes odds_change to EventProcessingWorker'
     xit 'passes fixtures_fixture to EventProcessingWorker'
     xit 'raises NotImplementedError on any unknown input'
