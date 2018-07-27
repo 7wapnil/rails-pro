efe4cb6531e2bd709d45be2a0794407301e2a1ea
Rahul <rmishra@gammastack.com>
2018-07-27 14:24:26 +0300

    Layout Heartbeat::Service spec

--- spec/services/heartbeat/service_spec.rb
+++ spec/services/heartbeat/service_spec.rb
@@ -9,5 +9,34 @@ describe Heartbeat::Service do
         .to respond_to(:call)
         .with_keywords(:client, :product, :timestamp, :subscribed)
     end
+
+    context 'subscription active' do
+      xit 'saves last successful timestamp for the product'
+    end
+
+    context 'subscription lost' do
+      describe 'rate limits' do
+        context 'rate limits not reached' do
+          xit 'sends recovery API call'
+          xit 'sends recovery API call with correct timestamp'
+          xit 'modifies last successful recovery call'
+        end
+        context 'rate limits reached' do
+          xit 'does not modify last successful recovery call'
+          xit 'ignores the request the job'
+        end
+      end
+
+      # xit 'does not send more than 2 Requests per minute: 1 per 30 seconds'
+      # xit 'does not send more than 600 Requests per hour: 1 per 6 seconds'
+      xit 'has minimal gap between two recovery calls'
+    end
+
+    context 'negative cases' do
+      xit 'does not handle invalid client'
+      xit 'does not handle invalid product'
+      xit 'does not handle invalid timestamp'
+      xit 'does not handle invalid subscribed'
+    end
   end
 end
