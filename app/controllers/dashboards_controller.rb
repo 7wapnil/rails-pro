c5cc504defc2eba235cbc22e3b45a9e9ed93bbce
Mahendra <mtyagi@gammastack.com>
2018-08-01 16:26:54 +0300

    Fix rubocop issues

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,11 +5,9 @@ class DashboardsController < ApplicationController
   def create
     client = WebSocketClient.new
     client.connect
-    client.emit('test', {
-      message: params[:message],
-      name: current_user.full_name
-    })
-    flash[:success] = "Message sent"
+    client.emit('test', message: params[:message],
+                        name: current_user.full_name)
+    flash[:success] = 'Message sent'
     redirect_to dashboard_path
   end
 end
