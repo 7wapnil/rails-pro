db1bb0602e98b2958e342458f6d5c89d51c00f46
Mahendra <mtyagi@gammastack.com>
2018-08-10 14:56:06 +0300

    Ignore Signal Exception type and error message

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,9 +1,16 @@
 Airbrake.configure do |config|
   config.host = 'https://arcanebet-errbit.herokuapp.com'
   config.project_id = 1
-  config.project_key = ENV['ERRBIT_PROJECT_KEY']
+  config.project_key = 'dfgdfg' # ENV['ERRBIT_PROJECT_KEY']
 
   config.environment = Rails.env
-  config.ignore_environments = %w[development test]
-  config.ignore << 'ActiveRecord::IgnoreThisError'
+  # config.ignore_environments = %w[development test]
+end
+
+Airbrake.add_filter do |notice|
+  if notice[:errors].any? do |error|
+       error[:type] == 'SignalException' && error[:message] == 'SIGTERM'
+     end
+    notice.ignore!
+  end
 end
