224faa6d20b96e66e13aaa9056f440b7d9a2b799
Mahendra <mtyagi@gammastack.com>
2018-08-14 09:42:13 +0300

    Fix errbit config

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,10 +1,10 @@
 Airbrake.configure do |config|
   config.host = 'https://arcanebet-errbit.herokuapp.com'
   config.project_id = 1
-  config.project_key = 'dfgdfg' # ENV['ERRBIT_PROJECT_KEY']
+  config.project_key = ENV['ERRBIT_PROJECT_KEY']
 
   config.environment = Rails.env
-  # config.ignore_environments = %w[development test]
+  config.ignore_environments = %w[development test]
 end
 
 Airbrake.add_filter do |notice|
