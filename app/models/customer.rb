5a4104fd5dde7dd7392ec422ebe39e33d9eefa62
Mahendra <mtyagi@gammastack.com>
2018-08-29 15:04:38 +0300

    Implement uploaded files validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -34,6 +34,12 @@ class Customer < ApplicationRecord
   has_one :address
   has_and_belongs_to_many :labels
 
+  validates :customer_attachment,
+            file_size: { less_than_or_equal_to: 2.megabytes,
+                         message: 'File is too big' },
+            file_content_type: { allow: 'image/jpeg',
+                                 message: 'Only jpeg, please' }
+
   # Devise Validatable module creates all needed
   # validations for a user email and password.
 
