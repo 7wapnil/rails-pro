bd4c14e21f418500df34a4800a883ac0f050f46b
Varun <vsharma@gammastack.com>
2018-09-04 12:29:45 +0300

    Define market priority autmatically based on name

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,13 @@
 class Market < ApplicationRecord
+  before_validation :define_priority, if: :name_changed?
+
+  PRIORITIES_MAP = [
+    [
+      /^Match winner/
+    ], # First priority
+    [] # Second priority
+  ].freeze
+
   STATUSES = {
     inactive: 0,
     active: 1,
@@ -16,4 +25,15 @@ class Market < ApplicationRecord
   has_many :odds
 
   validates :name, :priority, :status, presence: true
+
+  def define_priority
+    self.priority = 0
+    PRIORITIES_MAP.each_with_index do |regex_map, index|
+      regex_map.each do |regex|
+        next if name.scan(regex).empty?
+        self.priority = index + 1
+        break
+      end
+    end
+  end
 end
