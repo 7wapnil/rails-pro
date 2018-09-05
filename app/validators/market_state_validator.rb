ed7bf6bff562031b0860aa582a1d326aaea04f27
Varun <vsharma@gammastack.com>
2018-09-05 10:02:37 +0300

    Optimize restrictions search

--- app/validators/market_state_validator.rb
+++ app/validators/market_state_validator.rb
@@ -5,12 +5,12 @@ class MarketStateValidator < ActiveModel::Validator
     return if record.status_was.nil?
 
     change = record.status_change
-    options[:restrictions].each do |rule|
-      next unless rule[0].to_s == change[0]
-      next unless rule[1].to_s == change[1]
-      record.errors[:status] << I18n.t('errors.messages.wrong_market_state',
-                                       initial_state: change[0],
-                                       new_state: change[1])
+    return unless options[:restrictions].any? do |restriction|
+      restriction[0].to_s == change[0] && restriction[1].to_s == change[1]
     end
+
+    record.errors[:status] << I18n.t('errors.messages.wrong_market_state',
+                                     initial_state: change[0],
+                                     new_state: change[1])
   end
 end
