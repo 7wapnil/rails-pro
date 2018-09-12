c0ad3207e667c2a975df2dc29ccf02e8d70264e1
Mahendra <mtyagi@gammastack.com>
2018-09-12 11:10:31 +0300

    Implement customer upload documents

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,18 +5,18 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
+    @customer = find_customer_by_id
     @labels = Label.all
   end
 
   def account_management
-    @customer = Customer.find(params[:id])
+    @customer = find_customer_by_id
     @entry_request = EntryRequest.new(customer: @customer)
     @entry_requests = @customer.entry_requests.page(params[:page])
   end
 
   def activity
-    @customer = Customer.find(params[:id])
+    @customer = find_customer_by_id
     @entries = @customer.entries.page(params[:page])
     @audit_logs = AuditLog
                   .where(origin_kind: :customer,
@@ -25,17 +25,26 @@ class CustomersController < ApplicationController
   end
 
   def notes
-    @customer = Customer.find(params[:id])
+    @customer = find_customer_by_id
     @note = CustomerNote.new(customer: @customer)
     @customer_notes = @customer.customer_notes.page(params[:page]).per(5)
   end
 
   def documents
-    @customer = Customer.find(params[:id])
+    @customer = find_customer_by_id
+  end
+
+  def upload_documents
+    @customer = find_customer_by_id
+    documents_from_params.each do |attachment_type, file|
+      plural_attachment_type = attachment_type.pluralize
+      @customer.send(plural_attachment_type).attach(file)
+    end
+    redirect_to documents_customer_path(@customer)
   end
 
   def update_labels
-    customer = Customer.find(params[:id])
+    customer = find_customer_by_id
     if labels_params[:ids].include? '0'
       customer.labels.clear
     else
@@ -49,7 +58,12 @@ class CustomersController < ApplicationController
     params.require(:labels).permit(ids: [])
   end
 
-  def customer_attachment_upload_params
-    params.require(:customer).permit(:id, :customer_attachment)
+  def find_customer_by_id
+    Customer.find(params[:id])
+  end
+
+  def documents_from_params
+    params
+      .permit(*Customer::ATTACHMENT_TYPES)
   end
 end
