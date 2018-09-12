93403125aefd26e98518c1cbdbb19a072676e4a7
Mahendra <mtyagi@gammastack.com>
2018-09-12 16:12:31 +0300

    Fix uploading documents on staging

--- app/controllers/customer_attachment_controller.rb
+++ app/controllers/customer_attachment_controller.rb
@@ -4,8 +4,16 @@ class CustomerAttachmentController < ApiController
   respond_to :json
 
   def customer_attachment_upload
+    error_msg = 'Customer not found from token'
+    unless current_customer
+      return render(json: { success: false, errors: [error_msg] })
+    end
+
+    Rails.logger.debug("Uploading attachments for customer #{current_customer}")
+    Rails.logger.debug("received attachments #{params[:attachments].keys}")
+
     attachments_from_params.each do |attachment_type, file|
-      current_customer.send("#{attachment_type}=", file)
+      current_customer.send(attachment_type).attach(file)
     end
 
     unless current_customer.valid?
