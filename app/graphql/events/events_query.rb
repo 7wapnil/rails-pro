168f3949a3d23f441bf2ed27b2340cdcc3bd6f15
Rahul <rmishra@gammastack.com>
2018-09-13 17:19:42 +0300

    Add inPlay argument to events query

--- app/graphql/events/events_query.rb
+++ app/graphql/events/events_query.rb
@@ -6,18 +6,24 @@ module Events
 
     description 'Get all events'
 
+    argument :inPlay, types.Boolean
+
     def auth_protected?
       false
     end
 
     def resolve(_obj, args)
-      Event
-        .joins(markets: :odds)
-        .where(title: Title.find_by(name: 'Counter-Strike'))
-        .where(start_at: (3.hours.ago..10.hours.from_now))
-        .group('events.id')
-        .order(:start_at)
-        .limit(args[:limit])
+      query = Event
+              .joins(markets: :odds)
+              .where(title: Title.find_by(name: 'Counter-Strike'))
+              .where(start_at: (3.hours.ago..10.hours.from_now))
+              .group('events.id')
+              .order(:start_at)
+
+      query = query.limit(args[:limit]) if args[:limit]
+      query = query.in_play if args[:inPlay]
+
+      query
     end
   end
 end
