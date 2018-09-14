e16dfc1529dcd1258f8db364b73f1ef9ff406b01
Rahul <rmishra@gammastack.com>
2018-09-14 13:43:30 +0300

    Introduce ApplicationWorker

--- app/workers/radar/live_coverage_booking_worker.rb
+++ app/workers/radar/live_coverage_booking_worker.rb
@@ -1,7 +1,5 @@
 module Radar
-  class LiveCoverageBookingWorker
-    include Sidekiq::Worker
-
+  class LiveCoverageBookingWorker < ApplicationWorker
     def perform(event_external_id)
       Rails.logger.info "Booking event #{event_external_id} for live coverage"
       OddsFeed::Radar::Client.new.book_live_coverage(event_external_id)
