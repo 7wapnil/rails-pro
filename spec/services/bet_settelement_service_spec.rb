ad86726d1b0f0e9e99f6b87b57a3004dfad7a2fb
Rahul <rmishra@gammastack.com>
2018-09-18 11:15:37 +0300

    Extract spec for callable

--- spec/services/bet_settelement_service_spec.rb
+++ spec/services/bet_settelement_service_spec.rb
@@ -1,27 +1,7 @@
+require 'services/service_spec'
+
 describe BetSettelement::Service do
-  describe 'behaves like a service' do
-    it 'is callable with one argument' do
-      expect(described_class).to respond_to(:call).with(1).argument
-    end
-
-    it 'responds to call method' do
-      expect(described_class.new(double))
-        .to respond_to(:call).with(0).argument
-    end
-
-    it 'passes #call to instance method .call' do
-      arguments = double
-
-      expect(described_class)
-        .to receive(:new)
-        .with(arguments)
-        .and_call_original
-
-      expect_any_instance_of(described_class).to receive(:call)
-
-      described_class.call(arguments)
-    end
-  end
+  it_behaves_like 'callable service'
 
   describe 'initialize' do
     let(:bet) { create(:bet) }
