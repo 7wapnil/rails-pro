8a1b913b9860aa522fb6a7310c2afab48d8a8802
Rahul <rmishra@gammastack.com>
2018-09-19 11:07:37 +0300

    Introduce refund EntryKind

--- app/models/concerns/entry_kinds.rb
+++ app/models/concerns/entry_kinds.rb
@@ -5,14 +5,15 @@ module EntryKinds
     deposit: 0,
     win: 1,
     withdraw: 3,
-    bet: 4
+    bet: 4,
+    refund: 5
   }.freeze
 
   FUND_KINDS = KINDS.slice(:deposit, :withdraw)
 
-  TRADING_KINDS = KINDS.slice(:win, :bet)
+  TRADING_KINDS = KINDS.slice(:win, :bet, :refund)
 
-  DEBIT_KINDS = KINDS.slice(:deposit, :win)
+  DEBIT_KINDS = KINDS.slice(:deposit, :win, :refund)
 
   CREDIT_KINDS = KINDS.slice(:withdraw, :bet)
 
