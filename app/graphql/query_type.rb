f0c0d269d848992a5a393719c651be2c430c6f13
Varun <vsharma@gammastack.com>
2018-09-20 11:52:18 +0300

    Get rid of single entity graphQL endpoints

--- app/graphql/query_type.rb
+++ app/graphql/query_type.rb
@@ -3,15 +3,6 @@ QueryType = GraphQL::ObjectType.define do
 
   field :wallets, function: Wallets::WalletsQuery.new
   field :titles, function: Titles::TitlesQuery.new
-  field :title, function: Titles::TitleQuery.new
   field :events, function: Events::EventsQuery.new
   field :markets, function: Events::MarketsQuery.new
-  field :market, function: Events::MarketQuery.new
-
-  field :event do
-    type Events::EventType
-    description 'Get an event by id'
-    argument :id, !types.ID
-    resolve ->(_obj, args, _ctx) { Event.find(args['id']) }
-  end
 end
--- app/graphql/titles/titles_query.rb
+++ app/graphql/titles/titles_query.rb
@@ -4,6 +4,7 @@ module Titles
 
     description 'Get all titles'
 
+    argument :id, types.ID
     argument :kind, types.String
 
     def auth_protected?
@@ -12,6 +13,7 @@ module Titles
 
     def resolve(_obj, args)
       query = Title
+      query = query.where(id: args[:id]) if args[:id]
       query = query.where(kind: args[:kind]) if args[:kind]
       query.all
     end
