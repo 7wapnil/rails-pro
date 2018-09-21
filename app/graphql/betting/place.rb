9e786a69888d867ba1dd5a409b837b81c0c4823e
Rahul <rmishra@gammastack.com>
2018-09-21 14:22:41 +0300

    Exctract factory method to separate method

--- app/graphql/betting/place.rb
+++ app/graphql/betting/place.rb
@@ -6,16 +6,20 @@ module Betting
 
     def resolve(_obj, args)
       args[:bets].map do |bet_payload|
-        bet = Bet.create!(
-          customer: @current_customer,
-          odd: Odd.find(bet_payload[:oddId]),
-          currency: Currency.find_by!(code: bet_payload[:currencyCode]),
-          amount: bet_payload[:amount],
-          odd_value: bet_payload[:oddValue]
-        )
-
-        BetPlacement::Service.call(bet)
+        BetPlacement::SubmissionService.call(create_bet(bet_payload))
       end
+    end
+
+    private
+
+    def create_bet(bet_payload)
+      Bet.create!(
+        customer: @current_customer,
+        odd: Odd.find(bet_payload[:oddId]),
+        currency: Currency.find_by!(code: bet_payload[:currencyCode]),
+        amount: bet_payload[:amount],
+        odd_value: bet_payload[:oddValue]
+      )
     end
   end
 end
