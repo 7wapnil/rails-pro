abbe4986617c99609a269664351f930ce6cb083f
Varun <vsharma@gammastack.com>
2018-09-25 15:28:24 +0300

    Validate odd value before create

--- app/services/odds_feed/radar/market_generator.rb
+++ app/services/odds_feed/radar/market_generator.rb
@@ -68,12 +68,19 @@ module OddsFeed
       def generate_odd!(odd_data)
         odd_id = "#{market.external_id}:#{odd_data['id']}"
         Rails.logger.info "Updating odd with external ID #{odd_id}"
+        return unless odd_valid?(odd_id, odd_data)
         odd = Odd.find_or_initialize_by(external_id: odd_id,
                                         market: market)
         odd.assign_attributes(name: transpiler.odd_name(odd_data['id']),
                               status: odd_data['active'].to_i,
                               value: odd_data['odds'])
         odd.save!
+      end
+
+      def odd_valid?(odd_id, odd_data)
+        return true unless odd_data['odds'].blank?
+        Rails.logger.info "Odd value for odd #{odd_id} is empty"
+        false
       end
 
       def transpiler
