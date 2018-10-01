7c2f42f2d6c10b4c8dc47d3368b3b490c65083d4
Varun <vsharma@gammastack.com>
2018-10-01 15:33:28 +0300

    Remove testing config values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
 
   config.ssl_options = {
     redirect: { exclude: ->(request) { request.path =~ /health-check/ } }
