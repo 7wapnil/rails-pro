4ed9f43d5d6cccfca9cdb898f78d412e13b13483
Varun <vsharma@gammastack.com>
2018-10-03 16:27:42 +0300

    Show files validation errors

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,10 +1,9 @@
 module CustomersHelper
   def attachment_for(customer, kind)
-    collection = customer
-                 .verification_documents
-                 .where(kind: kind)
-                 .order(created_at: :desc)
-    return collection.first if collection.any?
-    OpenStruct.new(filename: t(:no_file))
+    customer
+      .verification_documents
+      .where(kind: kind)
+      .order(created_at: :desc)
+      .first
   end
 end
