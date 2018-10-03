23b76f35cf6bc1231a3747da58a7105da685959e
Varun <vsharma@gammastack.com>
2018-10-03 14:39:13 +0300

    Show filename on upload, submit button disabling on upload

---
+++ app/webpacker/packs/customers/documents_upload.js
@@ -0,0 +1,21 @@
+
+const initDocumentsUpload = () => {
+  $('input[type=file]').on('change', (e) => {
+    const files = e.target.files
+    if (files.length > 0) {
+      const name = $(e.target).attr('name')
+      $(`[data-target=${name}]`).text(files[0].name)
+    }
+  })
+
+  $('.upload-form').on('submit', () => {
+    $('[type=submit]').prop('disabled', true)
+  })
+}
+
+let docsLoaded = false
+if (!docsLoaded) {
+  document.addEventListener('turbolinks:load', initDocumentsUpload)
+  initDocumentsUpload()
+  docsLoaded = true
+}
