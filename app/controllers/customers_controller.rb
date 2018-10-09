9defe9c5ec40af7ddc1247f92d7a7dca26e8a292
Jay <jhardia@gammastack.com>
2018-10-09 15:08:03 +0300

    implemented customer activity sorting

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -17,7 +17,8 @@ class CustomersController < ApplicationController
 
   def activity
     @customer = find_customer
-    @entries = @customer.entries.page(params[:page])
+    @search = @customer.entries.search(query_params)
+    @entries = @search.result.page(params[:page])
     @audit_logs = AuditLog
                   .where(origin_kind: :customer,
                          origin_id: @customer.id)
