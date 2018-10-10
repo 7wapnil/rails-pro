d1d2eaf559a18e26a10a040fb70637acd192b725
Mahendra <mtyagi@gammastack.com>
2018-10-10 16:22:54 +0300

    Add change password graphql mutation

--- app/graphql/mutation_type.rb
+++ app/graphql/mutation_type.rb
@@ -3,6 +3,7 @@ MutationType = GraphQL::ObjectType.define do
 
   field :signIn, function: Account::SignIn.new
   field :signUp, function: Account::SignUp.new
+  field :changePassword, function: Account::ChangePassword.new
 
   field :placeBets, function: Betting::Place.new
   field :deleteFile, function: Documents::DeleteFile.new
