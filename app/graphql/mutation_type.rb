0a40ecef1ed2f7000a53dfda775105319ee40c53
Varun <vsharma@gammastack.com>
2018-10-10 15:54:44 +0300

    Create customer activation API endpoint

--- app/graphql/mutation_type.rb
+++ app/graphql/mutation_type.rb
@@ -3,6 +3,7 @@ MutationType = GraphQL::ObjectType.define do
 
   field :signIn, function: Account::SignIn.new
   field :signUp, function: Account::SignUp.new
+  field :activate, function: Account::Activate.new
 
   field :placeBets, function: Betting::Place.new
   field :deleteFile, function: Documents::DeleteFile.new
