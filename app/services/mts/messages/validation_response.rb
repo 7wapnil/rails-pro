e6eeba30a3ee919069af9feaa5e8422759f23720
Rahul <rmishra@gammastack.com>
2018-10-10 14:19:08 +0300

    Refactor ValidationResponse class
    move under MTS module
    set message back to hash instead of OpenStruct

---
+++ app/services/mts/messages/validation_response.rb
@@ -0,0 +1,51 @@
+module Mts
+  module Messages
+    class ValidationResponse
+      SUPPORTED_VALIDATION_RESPONSE_VERSION = '2.1'.freeze
+
+      attr_reader :message
+
+      def initialize(input_json)
+        @message = parse(input_json)
+        raise NotImplementedError unless version ==
+                                         SUPPORTED_VALIDATION_RESPONSE_VERSION
+      end
+
+      # getters
+
+      def version
+        @message[:version]
+      end
+
+      def bets
+        Bet.where(validation_ticket_id: ticket_id)
+      end
+
+      # getters.validation_result
+
+      def ticket_id
+        result[:ticket_id]
+      end
+
+      def accepted?
+        result[:status] == 'accepted'
+      end
+
+      def rejected?
+        result[:status] == 'rejected'
+      end
+
+      def result
+        @message[:result]
+      end
+
+      private
+
+      def parse(json)
+        hash = JSON.parse(json)
+        ::HashDeepFormatter
+          .deep_transform_keys(hash) { |key| key.to_s.underscore.to_sym }
+      end
+    end
+  end
+end
