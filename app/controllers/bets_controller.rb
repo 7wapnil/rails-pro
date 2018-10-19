98f257588104921e9d6037345d15f62a679ac51d
Dinesh <dinesh@gammastack.com>
2018-10-19 11:22:21 +0300

    Add bet filtering

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,10 +1,30 @@
 class BetsController < ApplicationController
   def index
+    if !query_params.nil? && query_params[:created_at_lt].present?
+      query_params[:created_at_lt] = query_params[:created_at_lt]
+                                     .to_date
+                                     .end_of_day
+    end
+    @dates = collect_query_dates
     @search = Bet.with_winnings.search(query_params)
     @bets = @search.result.page(params[:page])
   end
 
   def show
-    @bet = Bet.includes(%i[currency odd]).find(params[:id])
+    @bet = Bet
+           .includes(%i[currency customer odd])
+           .with_winnings
+           .find(params[:id])
+  end
+
+  private
+
+  def collect_query_dates
+    {
+      created_at_gteq:
+        !query_params.nil? ? query_params[:created_at_gteq] : nil,
+      created_at_lt:
+        !query_params.nil? ? query_params[:created_at_lt] : nil
+    }
   end
 end
