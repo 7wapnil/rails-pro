cd5d8c98468c049e8a04699550a807ff7285e93f
Jay <jhardia@gammastack.com>
2018-10-23 12:25:36 +0300

    Code cleanup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def index
     @search = Event.order(start_at: :desc).search(query_params)
     @events = @search.result.page(params[:page])
-    @sports = Title.all.uniq.pluck(:name)
+    @sports = Title.pluck(:name)
   end
 
   def show
