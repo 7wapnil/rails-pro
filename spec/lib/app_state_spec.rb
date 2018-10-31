03e63f51284095ed1f1f8c26188c215b0f61b20d
Rahul <rmishra@gammastack.com>
2018-10-31 09:47:30 +0200

    Refactor AppState spec

--- spec/lib/app_state_spec.rb
+++ spec/lib/app_state_spec.rb
@@ -1,16 +1,20 @@
 describe ApplicationState.instance do
-  it 'defines defaults status as active' do
-    expect(subject.status).to eq(:active)
+  context '.initialize' do
+    it 'defines defaults status as active' do
+      expect(subject.states[:status]).to eq(:active)
+    end
   end
 
-  it 'sends web socket event on status change' do
-    subject.status = :inactive
-    expect(WebSocket::Client.instance)
-      .to have_received(:emit)
-      .with(WebSocket::Signals::APP_STATE_UPDATED, anything)
-  end
+  context '.status=' do
+    it 'sends web socket event on status change' do
+      subject.status = :inactive
+      expect(WebSocket::Client.instance)
+        .to have_received(:emit)
+        .with(WebSocket::Signals::APP_STATE_UPDATED, anything)
+    end
 
-  it 'raises error on wrong status' do
-    expect { subject.status = :unknown }.to raise_error(StandardError)
+    it 'raises error on wrong status' do
+      expect { subject.status = :unknown }.to raise_error(StandardError)
+    end
   end
 end
