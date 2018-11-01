dd05080de967b2f0f4f836d96c8dac9cd37271d2
Varun <vsharma@gammastack.com>
2018-11-01 11:59:29 +0200

    Add reserved odd value test case

--- spec/models/odd_spec.rb
+++ spec/models/odd_spec.rb
@@ -7,9 +7,14 @@ describe Odd do
   it { should validate_presence_of(:status) }
   it { should validate_numericality_of(:value).is_greater_than(0) }
 
-  it do
+  it 'validates value on create is status active' do
     subject.status = 1
     should validate_presence_of(:value).on(:create)
+  end
+
+  it 'not validates value on create if status inactive' do
+    subject.status = 0
+    should_not validate_presence_of(:value).on(:create)
   end
 
   it_behaves_like 'has unique :external_id' do
