211b58e94b3945365dc7f24a97a3899c19a43385
Rahul <rmishra@gammastack.com>
2018-11-05 11:03:57 +0200

    Monkeypatch Sidekiq::Scheduler using module extension
    https://www.justinweiss.com/articles/3-ways-to-monkey-patch-without-making-a-mess/

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 require Rails.root.join('lib/sidekiq/silence_job_logger')
 require Rails.root.join('lib/sidekiq_scheduler/silent_scheduler')
 
+Sidekiq::Scheduler.include Sidekiq::SilentScheduler
+
 Sidekiq::Logging.logger = ::MaskedLogStashLoggerFactory.build(type: :stdout)
 
 Sidekiq.configure_server do |config|
@@ -8,7 +10,7 @@ Sidekiq.configure_server do |config|
     Sidekiq.schedule = YAML.load_file(
       File.expand_path('../sidekiq_scheduler.yml', __dir__)
     )
-    SidekiqScheduler::SilentScheduler.instance.reload_schedule!
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
   config.options[:job_logger] = SilenceJobLogger
 end
