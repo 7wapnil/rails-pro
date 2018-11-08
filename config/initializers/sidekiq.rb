8a36b3a4725988f602e34e1553d9f3ad2ccc7026
Dinesh <dinesh@gammastack.com>
2018-11-08 11:21:14 +0300

    Revert Sidekiq configuration cleanup commit

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,5 +6,11 @@ Sidekiq::Scheduler.include Sidekiq::SilentScheduler
 Sidekiq::Logging.logger = ::MaskedLogStashLoggerFactory.build(type: :stdout)
 
 Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(
+      File.expand_path('../sidekiq_scheduler.yml', __dir__)
+    )
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
+  end
   config.options[:job_logger] = Sidekiq::SilenceJobLogger
 end
