3fb5f7cc5b68d62a9761331da03e8256ffbdfeb8
Dinesh <dinesh@gammastack.com>
2018-11-09 09:21:43 +0300

    Sidekiq configuration cleanup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,8 @@
 require Rails.root.join('lib/sidekiq/silence_job_logger')
-require Rails.root.join('lib/sidekiq_scheduler/silent_scheduler')
-
-Sidekiq::Scheduler.include Sidekiq::SilentScheduler
 
 Sidekiq::Logging.logger = ::MaskedLogStashLoggerFactory.build(type: :stdout)
-Sidekiq::Logging.logger.level = ENV['RAILS_LOG_LEVEL'] || Logger::DEBUG
 
 Sidekiq.configure_server do |config|
-  config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(
-      File.expand_path('../sidekiq_scheduler.yml', __dir__)
-    )
-    SidekiqScheduler::Scheduler.instance.reload_schedule!
-  end
+  config.logger.level = ENV['SIDEKIQ_LOG_LEVEL'] || ::Logger::DEBUG
   config.options[:job_logger] = Sidekiq::SilenceJobLogger
 end
