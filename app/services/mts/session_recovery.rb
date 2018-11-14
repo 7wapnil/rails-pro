4674635797ae55f2ef57f90ece0915a243c68fc9
Rahul <rmishra@gammastack.com>
2018-11-14 15:09:22 +0200

    Fix SessionRecovery implementation

--- app/services/mts/session_recovery.rb
+++ app/services/mts/session_recovery.rb
@@ -1,35 +1,41 @@
 module Mts
   class SessionRecovery
-    def recover_from_network_failure!
-      return if session_failure_getter.present?
+    MTS_SESSION_FAILURE_KEY = :mts_session_failed_at
 
-      OddsFeed::Radar::Product.available_product_ids.each do |product_id|
-        producer = ::Radar::Producer.find_by_id(product_id)
-        producer.recover_subscription!
-      end
-      session_clear
+    def recover_from_network_failure!
+      return if session_failed_at.blank?
+
+      recover_producer_subscriptions!
+      clear_session_failed_at
     end
 
     def register_failure!
-      return if session_failure_getter.present?
+      return if session_failed_at.present?
 
-      timestamp = Time.now.getutc
-      session_failure(timestamp)
+      timestamp = Time.zone.now
+      update_session_failed_at(timestamp)
       Rails.logger.warn "Mts session failed at: #{timestamp}"
     end
 
     private
 
-    def session_failure(timestamp)
-      Rails.cache.write(:last_session_failure, timestamp)
+    def recover_producer_subscriptions!
+      OddsFeed::Radar::Product.available_product_ids.each do |product_id|
+        producer = Radar::Producer.find_by_id(product_id)
+        producer.recover_subscription!
+      end
     end
 
-    def session_failure_getter
-      Rails.cache.fetch(:last_session_failure)
+    def session_failed_at
+      Rails.cache.fetch(MTS_SESSION_FAILURE_KEY)
     end
 
-    def session_clear
-      Rails.cache.delete(:last_session_failure)
+    def update_session_failed_at(timestamp)
+      Rails.cache.write(MTS_SESSION_FAILURE_KEY, timestamp)
+    end
+
+    def clear_session_failed_at
+      Rails.cache.delete(MTS_SESSION_FAILURE_KEY)
     end
   end
 end
