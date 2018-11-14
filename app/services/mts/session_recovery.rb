98f299c186f44b5d5e55d9c9aaad8c925b2b8513
Rahul <rmishra@gammastack.com>
2018-11-14 10:31:08 +0200

    Fix rubocop offenses

--- app/services/mts/session_recovery.rb
+++ app/services/mts/session_recovery.rb
@@ -2,6 +2,7 @@ module Mts
   class SessionRecovery
     def recover_from_network_failure!
       return if session_failure_getter.present?
+
       OddsFeed::Radar::Product.available_product_ids.each do |product_id|
         producer = ::Radar::Producer.find_by_id(product_id)
         producer.recover_subscription!
@@ -11,6 +12,7 @@ module Mts
 
     def register_failure!
       return if session_failure_getter.present?
+
       timestamp = Time.now.getutc
       session_failure(timestamp)
       Rails.logger.warn "Mts session failed at: #{timestamp}"
