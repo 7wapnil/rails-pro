48d32eb9c0b828fd125674ef87dd61d7dba198c9
Dinesh <dinesh@gammastack.com>
2018-11-19 14:28:47 +0300

    Fix for PeriodScore resolver

--- app/graphql/types/period_score_type.rb
+++ app/graphql/types/period_score_type.rb
@@ -2,7 +2,7 @@ Types::PeriodScoreType = GraphQL::ObjectType.define do
   name 'PeriodScore'
 
   field :id, !types.ID do
-    resolve ->(obj, _args, _ctx) { obj['id'].to_id }
+    resolve ->(obj, _args, _ctx) { obj['id'].to_i }
   end
   field :score, !types.String do
     resolve ->(obj, _args, _ctx) { obj['score'] }
