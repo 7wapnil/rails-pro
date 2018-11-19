074f7817ca198aac7650e46a757cdcbeb1615cc2
Varun <vsharma@gammastack.com>
2018-11-19 14:47:39 +0200

    Make heartbeat and scopes loading jobs unique

--- app/workers/radar/missing_heartbeat_worker.rb
+++ app/workers/radar/missing_heartbeat_worker.rb
@@ -1,5 +1,8 @@
 module Radar
   class MissingHeartbeatWorker < ApplicationWorker
+    sidekiq_options lock: :until_executed,
+                    on_conflict: :log
+
     def perform
       Radar::Producer.available_producers.each(&:check_subscription_expiration)
     end
