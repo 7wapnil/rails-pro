be0415a8884311a3f395a01ca883ad1640b08d6f
Jay <jhardia@gammastack.com>
2018-11-19 10:41:10 +0200

    Cleanup BetsFilter specs

--- spec/lib/bets_filter_spec.rb
+++ spec/lib/bets_filter_spec.rb
@@ -1,25 +1,26 @@
 describe BetsFilter do
-  let(:usa) { 'USA' }
-  let(:xbet) { 'X-BET' }
-  let(:fifa) { 'FIFA' }
-
-  let(:filter) { described_class.new(Bet, {}) }
-  before do
-    create(:event_scope, kind: :country, name: usa)
-    create(:event_scope, kind: :tournament, name: xbet)
-    create(:title, name: fifa)
+  let!(:country) do
+    create(:event_scope, kind: :country, name: Faker::Address.country)
   end
 
+  let!(:tournament) do
+    create(:event_scope, kind: :tournament, name: Faker::Esport.league)
+  end
+
+  let!(:sport) { create(:title, name: Faker::Esport.game) }
+
+  let(:filter) { described_class.new(Bet, {}) }
+
   it 'returns sports' do
-    expect(filter.sports).to include(fifa)
+    expect(filter.sports).to include(sport.name)
   end
 
   it 'returns countries' do
-    expect(filter.countries).to match_array([usa])
+    expect(filter.countries).to match_array([country.name])
   end
 
   it 'returns tournaments' do
-    expect(filter.tournaments).to match_array([xbet])
+    expect(filter.tournaments).to match_array([tournament.name])
   end
 
   it 'returns ransack Search object' do
