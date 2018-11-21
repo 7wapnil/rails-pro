7b0de2ab15c8a11d55946679dca255e40f482a6d
Rahul <rmishra@gammastack.com>
2018-11-21 09:50:47 +0200

    Optimize sidekiq queue ordering

--- app/workers/radar/bet_settlement_worker.rb
+++ app/workers/radar/bet_settlement_worker.rb
@@ -1,6 +1,6 @@
 module Radar
   class BetSettlementWorker < BaseUofWorker
-    sidekiq_options retry: 0
+    sidekiq_options queue: :uof_priority, retry: 0
 
     def worker_class
       OddsFeed::Radar::BetSettlementHandler
