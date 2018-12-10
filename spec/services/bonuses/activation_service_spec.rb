ff4d5eebe615d3f37c7a9199bd97be29e20250e1
Jay <jhardia@gammastack.com>
2018-12-10 17:36:24 +0200

    Cover all activation_attrs with spec

--- spec/services/bonuses/activation_service_spec.rb
+++ spec/services/bonuses/activation_service_spec.rb
@@ -3,12 +3,21 @@ describe Bonuses::ActivationService do
   let(:wallet) { create(:wallet) }
 
   context 'bonus activation' do
+    include_context 'frozen_time'
     it 'creates new activated bonus' do
+      excluded = %i[created_at deleted_at updated_at id valid_for_days]
+      transmitted_attrs = ActivatedBonus.column_names & Bonus.column_names
+
+      original_bonus_attrs = transmitted_attrs.map(&:to_sym) - excluded
       activated_bonus = described_class.call(wallet, bonus)
 
       expect(activated_bonus).to be_instance_of(ActivatedBonus)
       expect(activated_bonus.customer_id).to eq(wallet.customer_id)
       expect(activated_bonus.original_bonus_id).to eq(bonus.id)
+      expect(activated_bonus.activated_at).to eq(Time.zone.now)
+      original_bonus_attrs.each do |bonus_attribute|
+        expect(activated_bonus[bonus_attribute]).to eq(bonus[bonus_attribute])
+      end
     end
 
     it 'removes previous bonus' do
