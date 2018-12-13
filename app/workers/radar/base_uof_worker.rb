25ee8d26e0c9cd00bda747166af744553c7f9426
Varun <vsharma@gammastack.com>
2018-12-13 18:15:13 +0200

    Pass thread id & job id to logger in workers

--- app/workers/radar/base_uof_worker.rb
+++ app/workers/radar/base_uof_worker.rb
@@ -3,8 +3,8 @@ module Radar
     sidekiq_options retry: 3
 
     def perform(payload, enqueued_at)
-      @enqueued_at = enqueued_at
-      @start_time = Time.now.to_f
+      super(enqueued_at)
+
       execute(payload)
       log_success
     rescue StandardError => e
@@ -21,35 +21,5 @@ module Radar
     def execute(payload)
       worker_class.new(XmlParser.parse(payload)).handle
     end
-
-    def log_success
-      msg = "#{self.class.name} successfully finished a job"
-      log_process(:info, msg)
-    end
-
-    def log_failure(error)
-      log_process(:error, error.message)
-    end
-
-    # rubocop:disable Metrics/MethodLength
-    def log_process(level, message)
-      current_time = Time.now.to_f
-      performing_time = @enqueued_at.zero? ? 0 : current_time - @enqueued_at
-      execution_time = current_time - @start_time
-      processing_time = performing_time + execution_time
-
-      Rails.logger.send(
-        level,
-        jid: jid,
-        worker: self.class.name,
-        message: message,
-        current_time: current_time,
-        job_enqueued_at: @enqueued_at,
-        job_performing_time: performing_time.round(3),
-        job_execution_time: execution_time.round(3),
-        overall_processing_time: processing_time.round(3)
-      )
-    end
-    # rubocop:enable Metrics/MethodLength
   end
 end
