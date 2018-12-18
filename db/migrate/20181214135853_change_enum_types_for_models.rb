e1e8aa40e5cc8d7275ae5ddad01469afbe8a0db2
Varun <vsharma@gammastack.com>
2018-12-18 16:37:59 +0200

    Fix postgresql error

--- db/migrate/20181214135853_change_enum_types_for_models.rb
+++ db/migrate/20181214135853_change_enum_types_for_models.rb
@@ -23,27 +23,58 @@ class ChangeEnumTypesForModels < ActiveRecord::Migration[5.2]
     change_column :verification_documents, :status,            :string
   end
 
-  def down # rubocop:disable Metrics/MethodLength
-    change_column :balances,               :kind,              :integer
-    change_column :bets,                   :settlement_status, :integer
-    change_column :bets,                   :status,            :integer
-    change_column :bonuses,                :kind,              :integer
-    change_column :customers,              :account_kind,      :integer
-    change_column :customers,              :gender,            :integer
-    change_column :customers,              :lock_reason,       :integer
-    change_column :customer_bonuses,       :kind,              :integer
-    change_column :entries,                :kind,              :integer
-    change_column :entry_currency_rules,   :kind,              :integer
-    change_column :entry_requests,         :kind,              :integer
-    change_column :entry_requests,         :mode,              :integer
-    change_column :entry_requests,         :status,            :integer
-    change_column :events,                 :status,            :integer
-    change_column :event_scopes,           :kind,              :integer
-    change_column :labels,                 :kind,              :integer
-    change_column :markets,                :status,            :integer
-    change_column :odds,                   :status,            :integer
-    change_column :titles,                 :kind,              :integer
-    change_column :verification_documents, :kind,              :integer
-    change_column :verification_documents, :status,            :integer
+  def down
+    remove_string_column
+    add_integer_column
+  end
+
+  private
+
+  def remove_string_column # rubocop:disable Metrics/MethodLength
+    remove_column :balances,               :kind
+    remove_column :bets,                   :settlement_status
+    remove_column :bets,                   :status
+    remove_column :bonuses,                :kind
+    remove_column :customers,              :account_kind
+    remove_column :customers,              :gender
+    remove_column :customers,              :lock_reason
+    remove_column :customer_bonuses,       :kind
+    remove_column :entries,                :kind
+    remove_column :entry_currency_rules,   :kind
+    remove_column :entry_requests,         :kind
+    remove_column :entry_requests,         :mode
+    remove_column :entry_requests,         :status
+    remove_column :events,                 :status
+    remove_column :event_scopes,           :kind
+    remove_column :labels,                 :kind
+    remove_column :markets,                :status
+    remove_column :odds,                   :status
+    remove_column :titles,                 :kind
+    remove_column :verification_documents, :kind
+    remove_column :verification_documents, :status
+  end
+
+  def add_integer_column # rubocop:disable Metrics/MethodLength
+    add_column :balances,               :kind,              :integer, default: 0
+    add_column :bets,                   :settlement_status, :integer, default: 0
+    add_column :bets,                   :status,            :integer, default: 0
+    add_column :bonuses,                :kind,              :integer, default: 0
+    add_column :customers,              :account_kind,      :integer, default: 0
+    add_column :customers,              :gender,            :integer, default: 0
+    add_column :customers,              :lock_reason,       :integer, default: 0
+    add_column :customer_bonuses,       :kind,              :integer, default: 0
+    add_column :entries,                :kind,              :integer, default: 0
+    add_column :entry_currency_rules,   :kind,              :integer, default: 0
+    add_column :entry_requests,         :kind,              :integer, default: 0
+    add_column :entry_requests,         :mode,              :integer, default: 0
+    add_column :entry_requests,         :status,            :integer, default: 0
+    add_column :events,                 :status,            :integer, default: 0
+    add_column :event_scopes,           :kind,              :integer, default: 0
+    add_column :labels,                 :kind,              :integer, default: 0
+    add_column :markets,                :status,            :integer, default: 0
+    add_column :odds,                   :status,            :integer, default: 0
+    add_column :titles,                 :kind,              :integer, default: 0
+    add_column :verification_documents, :kind,              :integer, default: 0
+    add_column :verification_documents, :status,            :integer, default: 0
   end
 end
