80c03afeededf6f9b8760421bb7f98f115947e78
Rahul <rmishra@gammastack.com>
2018-12-19 16:49:25 +0200

    Add functionality to update market template category

--- app/controllers/market_templates_controller.rb
+++ app/controllers/market_templates_controller.rb
@@ -3,4 +3,16 @@ class MarketTemplatesController < ApplicationController
     @search = MarketTemplate.ransack(query_params)
     @market_templates = @search.result.page(params[:page])
   end
+
+  def update
+    @market_template = MarketTemplate.find(params[:id])
+    @market_template.update!(market_template_params)
+    respond_to :js
+  end
+
+  private
+
+  def market_template_params
+    params.require(:market_template).permit(:category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -85,7 +85,7 @@ Rails.application.routes.draw do
     resources :markets, only: :update
   end
 
-  resources :market_templates, only: :index
+  resources :market_templates, only: %i[index update]
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
