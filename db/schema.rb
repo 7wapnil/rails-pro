fca596ac62ff30a0e2973cd025e85ec8d5929ff0
Rahul <rmishra@gammastack.com>
2018-12-20 15:53:55 +0200

    Add category to markets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_112735) do
+ActiveRecord::Schema.define(version: 2018_12_20_133922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -328,6 +328,7 @@ ActiveRecord::Schema.define(version: 2018_12_18_112735) do
     t.string "external_id"
     t.string "status"
     t.boolean "visible", default: true
+    t.string "category"
     t.index ["event_id"], name: "index_markets_on_event_id"
     t.index ["external_id"], name: "index_markets_on_external_id", unique: true
   end
