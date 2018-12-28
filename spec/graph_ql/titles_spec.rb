e889e3cc5cd6e19660160439490c4a75c5173b87
Varun <vsharma@gammastack.com>
2018-12-28 16:48:13 +0200

    Revert - [630] Show only sports and tournaments with events

--- spec/graph_ql/titles_spec.rb
+++ spec/graph_ql/titles_spec.rb
@@ -12,11 +12,10 @@ describe GraphQL, '#titles' do
       let(:query) { %({ titles { id name } }) }
 
       before do
-        create_list(:title, 5, :with_event)
         create_list(:title, 5)
       end
 
-      it 'returns list of titles with active events' do
+      it 'returns list of titles' do
         expect(result['data']['titles'].count).to eq(5)
       end
 
@@ -41,8 +40,8 @@ describe GraphQL, '#titles' do
       end
 
       before do
-        create_list(:title, 5, :with_event, kind: :sports)
-        create_list(:title, 5, :with_event, kind: :esports)
+        create_list(:title, 5, kind: :sports)
+        create_list(:title, 5, kind: :esports)
       end
 
       it 'returns esports titles list' do
@@ -62,33 +61,21 @@ describe GraphQL, '#titles' do
             }
         })
       end
-      let(:title)          { create(:title, :with_event) }
-      let(:control_count)  { rand(1..3) }
-      let(:event)          { title.events.first }
-      let(:inactive_event) { create(:event, :inactive) }
+      let(:title) { create(:title) }
 
       before do
-        create_list(:event_scope, control_count,
-                    events: [event],
-                    kind:   EventScope::TOURNAMENT,
-                    title:  title)
-
-        create_list(:event_scope, 3,
-                    events: [inactive_event],
-                    kind:  EventScope::TOURNAMENT,
-                    title: title)
+        create_list(:event_scope, 3, kind: EventScope::TOURNAMENT, title: title)
       end
 
       it 'returns titles with tournaments' do
         expect(result['data']).not_to be_nil
         expect(result['data']['titles'].count).to eq(1)
-        expect(result['data']['titles'].first['tournaments'].count)
-          .to eq(control_count)
+        expect(result['data']['titles'][0]['tournaments'].count).to eq(3)
       end
     end
 
     context 'single title' do
-      let(:title) { create(:title, :with_event) }
+      let(:title) { create(:title) }
       let(:query) do
         %({
             titles (id: #{title.id}) {
@@ -100,6 +87,38 @@ describe GraphQL, '#titles' do
       it 'returns single title' do
         expect(result['data']['titles'].count).to eq(1)
         expect(result['data']['titles'][0]['id']).to eq(title.id.to_s)
+      end
+    end
+
+    context 'with amounts' do
+      let(:title) { create(:title) }
+      let(:query) do
+        %({
+            titles {
+              id
+              eventsAmount
+              hasLive
+            }
+        })
+      end
+
+      before do
+        create(:event,
+               title: title,
+               start_at: Time.now,
+               end_at: nil,
+               traded_live: false)
+        create(:event,
+               title: title,
+               start_at: Time.now,
+               end_at: nil,
+               traded_live: true)
+      end
+
+      it 'returns titles with amounts' do
+        expect(result['data']['titles'].count).to eq(1)
+        expect(result['data']['titles'][0]['eventsAmount']).to eq(2)
+        expect(result['data']['titles'][0]['hasLive']).to eq(true)
       end
     end
   end
