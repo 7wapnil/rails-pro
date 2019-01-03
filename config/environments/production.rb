c0435224b7c40664f08069d17fac4a2fa22b06c0
Varun <vsharma@gammastack.com>
2019-01-03 14:03:57 +0200

    Add action cable allowed origins env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,10 +47,8 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins =
-  #   [ 'http://example.com', /http:\/\/example.*/ ]
+  config.allowed_request_origins = ENV.fetch('AC_ALLOWED_ORIGINS', '')
+                                      .split(',')
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
