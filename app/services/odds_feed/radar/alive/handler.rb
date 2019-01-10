0c787b895ec7448a81ae3d17227b3baf8c207133
Rahul <rmishra@gammastack.com>
2019-01-10 14:15:08 +0200

    Refactor Alive message handler to improve readability

--- app/services/odds_feed/radar/alive/handler.rb
+++ app/services/odds_feed/radar/alive/handler.rb
@@ -2,14 +2,20 @@ module OddsFeed
   module Radar
     module Alive
       class Handler < RadarMessageHandler
+        delegate :product, to: :message
+
         def handle
-          message = Message.new(@payload['alive'])
           return false if message.expired?
 
-          product = message.product
           return product.unsubscribe! unless message.subscribed?
 
           product.subscribed!(subscribed_at: message.received_at)
+        end
+
+        private
+
+        def message
+          Message.new(@payload['alive'])
         end
       end
     end
