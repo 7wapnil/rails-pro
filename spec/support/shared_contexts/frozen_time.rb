8409fed121a62f024a67ea8a4545b96a42510f32
Rahul <rmishra@gammastack.com>
2019-01-10 09:32:26 +0200

    Fix frozen_time sharted context
    fix according to https://gitlab.com/travisjeffery/timecop/issues/146

--- spec/support/shared_contexts/frozen_time.rb
+++ spec/support/shared_contexts/frozen_time.rb
@@ -1,5 +1,12 @@
 shared_context 'frozen_time' do
-  before { Timecop.freeze }
+
+  before do
+    # TODO: Revert to Timecop.freeze when precision fixed
+    # https://github.com/travisjeffery/timecop/issues/146
+    rounded_random_time =
+      Time.at(Random.rand * Time.now.to_i).round
+    Timecop.freeze(rounded_random_time)
+  end
 
   after { Timecop.return }
 end
