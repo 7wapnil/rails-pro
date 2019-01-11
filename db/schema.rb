7590d5bd87ac98ea6ee8e8e7c6802f13ab45d126
Varun <vsharma@gammastack.com>
2019-01-11 13:37:57 +0200

    Update subscription channel

--- db/schema.rb
+++ db/schema.rb
@@ -79,8 +79,8 @@ ActiveRecord::Schema.define(version: 2018_12_20_133922) do
     t.datetime "updated_at", null: false
     t.decimal "void_factor", precision: 2, scale: 1
     t.string "validation_ticket_id"
-    t.datetime "validation_ticket_sent_at"
     t.string "settlement_status"
+    t.datetime "validation_ticket_sent_at"
     t.bigint "customer_bonus_id"
     t.decimal "ratio", precision: 8, scale: 2
     t.index ["currency_id"], name: "index_bets_on_currency_id"
@@ -156,9 +156,9 @@ ActiveRecord::Schema.define(version: 2018_12_20_133922) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "expiration_reason"
     t.decimal "rollover_balance", precision: 8, scale: 2
     t.decimal "rollover_initial_value", precision: 8, scale: 2
-    t.string "expiration_reason"
     t.index ["customer_id"], name: "index_customer_bonuses_on_customer_id"
     t.index ["deleted_at"], name: "index_customer_bonuses_on_deleted_at"
     t.index ["wallet_id"], name: "index_customer_bonuses_on_wallet_id"
