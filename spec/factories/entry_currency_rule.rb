2f17d16bbd30faa2d357f6fa4db53d2e0f7d5ca1
Rahul <rmishra@gammastack.com>
2019-01-16 01:18:21 +0200

    Fix EntryCurrencyRule factory

--- spec/factories/entry_currency_rule.rb
+++ spec/factories/entry_currency_rule.rb
@@ -2,10 +2,21 @@
 
 FactoryBot.define do
   factory :entry_currency_rule do
-    kind       { EntryRequest.kinds.keys.first }
+    kind       { EntryRequest::DEPOSIT }
     min_amount { Faker::Number.decimal(1, 2) }
     max_amount { Faker::Number.decimal(4, 2) }
 
     currency
+
+    initialize_with do
+      EntryCurrencyRule
+        .where(currency: currency, kind: kind)
+        .first_or_create do |rule|
+          rule.update(
+            min_amount: min_amount,
+            max_amount: max_amount
+          )
+        end
+    end
   end
 end
