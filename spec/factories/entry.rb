ade5922a317fce90c7e6996a124893bdc2bcca03
Rahul <rmishra@gammastack.com>
2019-01-16 01:18:05 +0200

    Fix Entry factory

--- spec/factories/entry.rb
+++ spec/factories/entry.rb
@@ -2,10 +2,16 @@
 
 FactoryBot.define do
   factory :entry do
-    kind          { EntryRequest.kinds.keys.first }
+    kind          { Entry::DEPOSIT }
     amount        { Faker::Number.decimal(3, 2) }
     authorized_at { nil }
 
     wallet
+
+    before(:create) do |entry|
+      create(:entry_currency_rule,
+             currency: entry.currency,
+             kind: entry.kind)
+    end
   end
 end
