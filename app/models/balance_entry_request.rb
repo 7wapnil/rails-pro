3f51f34033cdf422997ecf7fd62ea7c9e70e8418
Jay <jhardia@gammastack.com>
2019-01-16 10:44:36 +0200

    Add balance association to BalanceEntryRequest

--- app/models/balance_entry_request.rb
+++ app/models/balance_entry_request.rb
@@ -1,7 +1,7 @@
 class BalanceEntryRequest < ApplicationRecord
   belongs_to :entry_request
   belongs_to :balance_entry, optional: true
-
+  has_one :balance, through: :balance_entry
   enum kind: Balance.kinds
 
   validates :amount, numericality: true, presence: true
