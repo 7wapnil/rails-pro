522c2e2d338a180009975ab9fcc8208bf19c3abc
Varun <vsharma@gammastack.com>
2019-01-17 11:17:39 +0200

    Implement markets list update subscriptions and signals

--- app/graphql/subscription_fields.rb
+++ app/graphql/subscription_fields.rb
@@ -4,4 +4,7 @@ module SubscriptionFields
   KIND_EVENT_UPDATED = :kind_event_updated
   SPORT_EVENT_UPDATED = :sport_event_updated
   TOURNAMENT_EVENT_UPDATED = :tournament_event_updated
+  MARKET_UPDATED = :market_updated
+  EVENT_MARKET_UPDATED = :event_market_updated
+  CATEGORY_MARKET_UPDATED = :category_market_updated
 end
