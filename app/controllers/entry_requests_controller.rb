f5475bf332e3c3fb801c984e6e4cb4305c80f9da
Jay <jhardia@gammastack.com>
2019-01-25 14:44:24 +0200

    Handle deposits with cashier and simulated mode via single entry request form

--- app/controllers/entry_requests_controller.rb
+++ app/controllers/entry_requests_controller.rb
@@ -12,18 +12,18 @@ class EntryRequestsController < ApplicationController
   end
 
   def create
-    entry_request = EntryRequest.new(payload_params)
+    customer = Customer.find(payload_params[:customer_id])
+    @entry_request = if deposit_simulation?
+                       handle_deposit_simulation(customer)
+                     else
+                       handle_entry_request_creation
+                     end
 
-    if entry_request.save
-      EntryRequestProcessingWorker.perform_async(entry_request.id)
-      customer = entry_request.customer
-      current_user.log_event :entry_request_created,
-                             entry_request,
-                             customer
-      flash[:success] = t('messages.entry_request.flash')
+    if @entry_request
+      current_user.log_event :entry_request_created, @entry_request, customer
+
       redirect_to account_management_customer_path(customer)
     else
-      flash[:error] = entry_request.errors.full_messages
       redirect_back fallback_location: root_path
     end
   end
@@ -35,5 +35,39 @@ class EntryRequestsController < ApplicationController
       .require(:entry_request)
       .permit(:customer_id, :currency_id, :amount, :kind, :mode, :comment)
       .merge(initiator: current_user)
+  end
+
+  def deposit_simulation?
+    payload_params[:kind] == EntryRequest::DEPOSIT &&
+      payload_params[:mode] == EntryRequest::SIMULATED
+  end
+
+  def handle_entry_request_creation
+    entry_request = EntryRequest.new(payload_params)
+
+    if entry_request.save
+      EntryRequestProcessingWorker.perform_async(entry_request.id)
+      flash[:success] = t('messages.entry_request.flash')
+      entry_request
+    else
+      flash[:error] = entry_request.errors.full_messages
+      nil
+    end
+  end
+
+  def handle_deposit_simulation(customer)
+    amount = payload_params[:amount].to_f
+    wallet = Wallet.find_or_create_by!(
+      customer: customer,
+      currency_id: payload_params[:currency_id]
+    )
+    payload = payload_params.slice(:comment, :mode, :initiator)
+                            .to_h
+                            .symbolize_keys
+
+    entry_request = Deposits::PlacementService.call(wallet, amount, **payload)
+    flash[:success] = I18n.t('events.deposit_created') if entry_request
+    flash[:error] = I18n.t('events.deposit_failed') unless entry_request
+    entry_request
   end
 end
