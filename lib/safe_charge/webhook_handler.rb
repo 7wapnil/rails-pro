3d88984a247d87f2223e43bd2f6fd921913ebd5b
Rahul <rmishra@gammastack.com>
2019-02-15 13:22:32 +0200

    Refactor webhook controller

--- lib/safe_charge/webhook_handler.rb
+++ lib/safe_charge/webhook_handler.rb
@@ -1,25 +1,25 @@
 module SafeCharge
   class WebhookHandler < ApplicationService
     def initialize(params)
-      @reply = reply(params)
+      @params = params
     end
 
     def call
-      @reply.verify_checksum!
-      return entry_request.succeeded! if @reply.approved?
-      return entry_request.pending! if @reply.pending?
+      reply.verify_checksum!
+      return entry_request.succeeded! if reply.approved?
+      return entry_request.pending! if reply.pending?
 
       entry_request.failed!
     end
 
     private
 
-    def reply(params)
-      SafeCharge::Reply.new(params)
+    def reply
+      @reply ||= SafeCharge::Reply.new(@params)
     end
 
     def entry_request
-      @entry_request ||= @reply.entry_request
+      @entry_request ||= reply.entry_request
     end
   end
 end
