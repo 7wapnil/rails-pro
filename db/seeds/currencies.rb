e027c87d81a329a4838c91e5c333d9b05e1a9053
Rahul <rmishra@gammastack.com>
2019-02-20 08:51:41 +0200

    Update seeds with currency kind

--- db/seeds/currencies.rb
+++ db/seeds/currencies.rb
@@ -1,10 +1,10 @@
 puts 'Checking currencies ...'
 
 currency_mapping = [
-  { name: 'Euro', code: 'EUR', primary: true },
-  { name: 'Swedish Kronor', code: 'SEK' },
-  { name: 'BitCoin', code: 'mBTC' },
-  { name: 'Ethereum', code: 'ETH' }
+  { name: 'Euro', code: 'EUR', primary: true, kind: 'fiat' },
+  { name: 'Swedish Kronor', code: 'SEK', kind: 'fiat' },
+  { name: 'BitCoin', code: 'mBTC', kind: 'crypto' },
+  { name: 'Ethereum', code: 'ETH', kind: 'crypto' }
 ]
 
 entry_currency_rule_ranges = {
@@ -20,6 +20,7 @@ currency_mapping.each do |payload|
   Currency.find_or_create_by(name: payload[:name]) do |currency|
     currency.code = payload[:code]
     currency.primary = payload[:primary] || false
+    currency.kind = payload[:kind]
   end
 end
 
