29e066e0fb9ee979d61524090f8f98eb20b1f2a2
Varun <vsharma@gammastack.com>
2019-03-06 12:49:42 +0200

    Fix for populate_job_info_to_thread usage error

--- lib/sidekiq/patched_processor.rb
+++ lib/sidekiq/patched_processor.rb
@@ -3,7 +3,7 @@
 module Sidekiq
   module PatchedProcessor
     def execute_job(worker, cloned_args)
-      worker.populate_job_info_to_thread
+      worker.populate_job_info_to_thread if worker.is?(::ApplicationWorker)
       worker.perform(*cloned_args)
     rescue StandardError => e
       worker.log_job_failure(e) if log_job_failure?(worker)
