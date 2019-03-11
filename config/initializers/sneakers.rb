b5ce4a7177db348a707239f671c206df0a1fc61f
Varun <vsharma@gammastack.com>
2019-03-11 11:49:42 +0200

    Replace heartbeat interval with network recovery interval for Sneakers

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -4,13 +4,15 @@ sneakers_logger = ::MaskedLogStashLoggerFactory.build(type: :stdout)
 
 sneakers_logger.level = ENV['RAILS_LOG_LEVEL'] || :debug
 
-Sneakers.configure heartbeat: 15,
-                   workers: 1,
+Sneakers.configure workers: 1,
                    log: sneakers_logger,
                    daemonize: false,
-                   connection: Bunny.new(host: ENV['RADAR_MQ_HOST'],
-                                         user: ENV['RADAR_MQ_USER'],
-                                         vhost: ENV['RADAR_MQ_VHOST'],
-                                         tls: ENV['RADAR_MQ_TLS'] == 'true')
+                   connection: Bunny.new(
+                     host: ENV['RADAR_MQ_HOST'],
+                     user: ENV['RADAR_MQ_USER'],
+                     vhost: ENV['RADAR_MQ_VHOST'],
+                     tls: ENV['RADAR_MQ_TLS'] == 'true',
+                     network_recovery_interval: 15
+                   )
 
 Sneakers.logger = sneakers_logger
