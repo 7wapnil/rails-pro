3e3a8d15a553e804e9c4cf6b4423cb209e9c5a6f
Varun <vsharma@gammastack.com>
2019-03-12 11:22:52 +0200

    Re-write useless test case for CompetitorLoader

--- spec/services/odds_feed/radar/entities/competitor_loader_spec.rb
+++ spec/services/odds_feed/radar/entities/competitor_loader_spec.rb
@@ -6,8 +6,9 @@ describe OddsFeed::Radar::Entities::CompetitorLoader do
   let(:payload) do
     XmlParser.parse(file_fixture('radar_competitor_profile.xml').read)
   end
-  let(:competitor_xml) { payload.dig('competitor_profile', 'competitor') }
-  let(:external_id) { competitor_xml['id'] }
+  let(:competitor_profile_from_xml) { payload['competitor_profile'] }
+  let(:competitor_from_xml) { competitor_profile_from_xml['competitor'] }
+  let(:external_id) { competitor_from_xml['id'] }
 
   let(:cache_settings) do
     {
@@ -16,7 +17,7 @@ describe OddsFeed::Radar::Entities::CompetitorLoader do
   end
 
   let(:name) do
-    competitor_xml['name']
+    competitor_from_xml['name']
       .split(described_class::NAME_SEPARATOR)
       .reverse
       .join(' ')
@@ -27,19 +28,21 @@ describe OddsFeed::Radar::Entities::CompetitorLoader do
       .to receive(:competitor_profile)
       .with(external_id, cache_settings)
       .and_return(payload)
+
+    allow(competitor_profile_from_xml)
+      .to receive(:dig)
+      .and_call_original
   end
 
-  context 'with cached player' do
-    let(:name) { Faker::WorldOfWarcraft.hero }
-
+  context 'with cached competitor' do
     before { allow(Rails.cache).to receive(:read).and_return(name) }
 
-    it 'takes player from cache' do
+    it 'takes competitor from cache' do
       expect(Rails.cache).to receive(:read)
       subject.call
     end
 
-    it 'returns player name' do
+    it 'returns competitor name' do
       expect(subject.call).to eq(name)
     end
 
@@ -48,22 +51,25 @@ describe OddsFeed::Radar::Entities::CompetitorLoader do
       subject.call
     end
 
-    it 'does not try to cache players' do
-      expect(payload).not_to receive(:dig).with('players', 'player')
+    it 'does not try to read and cache players from payload' do
+      expect(competitor_profile_from_xml)
+        .not_to receive(:dig)
+        .with('players', 'player')
+      subject.call
     end
   end
 
-  context 'with new player from Radar API' do
+  context 'with new competitor from Radar API' do
     let(:subject_with_name) { described_class.new(external_id: external_id) }
 
     before { allow(Rails.cache).to receive(:write) }
 
-    it 'caches player' do
+    it 'caches competitor' do
       expect(Rails.cache).to receive(:write)
       subject_with_name.call
     end
 
-    it 'returns player name' do
+    it 'returns competitor name' do
       expect(subject_with_name.call).to eq(name)
     end
 
@@ -79,6 +85,13 @@ describe OddsFeed::Radar::Entities::CompetitorLoader do
       let(:payload) do
         XmlParser
           .parse(file_fixture('radar_team_sport_competitor_profile.xml').read)
+      end
+
+      it 'tries to read and cache players from payload' do
+        expect(competitor_profile_from_xml)
+          .to receive(:dig)
+          .with('players', 'player')
+        subject.call
       end
 
       it_behaves_like 'service caches competitors and players' do
