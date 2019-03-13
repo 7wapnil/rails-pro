aa48fd513a1e972f8c3c28b3d5246c5bc1d76333
Jay <jhardia@gammastack.com>
2019-03-13 19:00:08 +0300

    Fix coding style violations

--- app/graphql/deposits/deposit_bonus_query.rb
+++ app/graphql/deposits/deposit_bonus_query.rb
@@ -17,21 +17,18 @@ module Deposits
     argument :code, !types.String
 
     def resolve(_obj, args)
-      bonus = find_bonus(args)
+      bonus = find_bonus!(args)
       bonus_hash = BalanceCalculations::Deposit.call(bonus, args[:amount])
       CalculatedBonus.new(bonus_hash)
     end
 
     private
 
-    def find_bonus(args)
-      model = Bonus.active.find_by(code: args[:code])
-
-      unless model
-        message = I18n.t('not_found') % { instance: 'bonus' }
-        raise ActiveRecord::RecordNotFound.new(message)
-      end
-      model
+    def find_bonus!(args)
+      Bonus.active.find_by!(code: args[:code])
+    rescue ActiveRecord::RecordNotFound
+      message = format(I18n.t('not_found'), instance: 'bonus')
+      raise ActiveRecord::RecordNotFound.new, message
     end
   end
 end
