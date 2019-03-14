7c2e280a664dfeebbd215ffb4f943ac94886d832
Jay <jhardia@gammastack.com>
2019-03-14 14:50:57 +0300

    Add a check of SafeCharge callback to match the corresponding EntryRequest

--- spec/services/safe_charge/deposit_response_spec.rb
+++ spec/services/safe_charge/deposit_response_spec.rb
@@ -6,13 +6,11 @@ describe SafeCharge::DepositResponse do
   let(:transaction_time_at_formatted) do
     transaction_time_at.strftime('%Y-%m-%d.%T')
   end
-  let(:total_amount) { Faker::Number.decimal(2, 2) }
-  let(:currency_code) { Faker::Currency.code }
   let(:transaction_id) { Faker::Number.number(9) }
   let(:transaction_status) { SafeCharge::Statuses::APPROVED }
-  let(:product_id) do
-    entry_request.id.to_s
-  end
+  let(:total_amount) { entry_request.amount }
+  let(:currency_code) { entry_request.currency.code }
+  let(:product_id) { entry_request.id.to_s }
 
   let(:correct_checksum) do
     checksum_string = [
@@ -42,14 +40,15 @@ describe SafeCharge::DepositResponse do
   end
 
   let(:entry_request) do
-    build_stubbed(:entry_request,
-                  kind: EntryRequest::DEPOSIT,
-                  status: EntryRequest::INITIAL)
+    create(:entry_request,
+           kind: EntryRequest::DEPOSIT,
+           status: EntryRequest::INITIAL)
   end
 
   before do
-    allow(ENV).to receive(:[])
-      .with('SAFECHARGE_SECRET_KEY').and_return(secret_key)
+    # allow(ENV).to receive(:[])
+    #   .with('SAFECHARGE_SECRET_KEY').and_return(secret_key)
+    ENV['SAFECHARGE_SECRET_KEY'] = secret_key
     allow(EntryRequest)
       .to receive(:find).with(entry_request.id).and_return(entry_request)
   end
@@ -90,6 +89,33 @@ describe SafeCharge::DepositResponse do
       it 'raises type error' do
         expect { verification_result }
           .to raise_error(described_class::TYPE_ERROR)
+      end
+    end
+
+    context 'with no parameters mismatch' do
+      it 'raises no exception' do
+        expect { verification_result }
+          .not_to raise_error(SafeCharge::CallbackDataMismatch)
+      end
+    end
+
+    # In the following examples data is changed on our side,
+    # because the process of checksum calculation and comparison
+    # is quite hard to intrude and alter
+
+    context 'with callback amount mismatching entry_request' do
+      it 'raises SafeCharge::CallbackDataMismatch' do
+        entry_request.update(amount: params['totalAmount'] / 2.0)
+        expect { verification_result }
+          .to raise_error(SafeCharge::CallbackDataMismatch)
+      end
+    end
+
+    context 'with callback currency code mesmatching entry_request currency' do
+      it 'raises SafeCharge::CallbackDataMismatch' do
+        entry_request.currency.update(code: params['currency'] + '_')
+        expect { verification_result }
+          .to raise_error(SafeCharge::CallbackDataMismatch)
       end
     end
   end
