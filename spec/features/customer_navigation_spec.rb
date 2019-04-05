e009e405a945f6cb5778fe9564922bceb23721bd
Rahul <rmishra@gammastack.com>
2019-04-05 10:33:16 +0300

    Add some missing tests for customer navigation links

--- spec/features/customer_navigation_spec.rb
+++ spec/features/customer_navigation_spec.rb
@@ -17,10 +17,25 @@ context 'Customers#show navigation' do
     end
   end
 
+  it 'navigates to customers#account_management' do
+    within 'ul.navigation' do
+      click_link I18n.t('navigation.customer.account_management')
+      expect(page)
+        .to have_current_path(account_management_customer_path(customer))
+    end
+  end
+
   it 'navigates to customers#activity' do
     within 'ul.navigation' do
       click_link I18n.t('navigation.customer.activity')
       expect(page).to have_current_path(activity_customer_path(customer))
+    end
+  end
+
+  it 'navigates to customers#bonuses' do
+    within 'ul.navigation' do
+      click_link I18n.t('navigation.customer.bonuses')
+      expect(page).to have_current_path(bonuses_customer_path(customer))
     end
   end
 
@@ -31,12 +46,35 @@ context 'Customers#show navigation' do
     end
   end
 
+  it 'navigates to customers#documents' do
+    within 'ul.navigation' do
+      click_link I18n.t('navigation.customer.documents')
+      expect(page).to have_current_path(documents_customer_path(customer))
+    end
+  end
+
   it 'navigates to customers#bets' do
     within 'ul.navigation' do
       click_link I18n.t('navigation.customer.bets')
       expect(page).to have_current_path(bets_customer_path(customer))
     end
   end
+
+  it 'navigates to customers#betting_limits' do
+    within 'ul.navigation' do
+      click_link I18n.t('navigation.customer.betting_limits')
+      expect(page).to have_current_path(betting_limits_customer_path(customer))
+    end
+  end
+
+  # FIXME: works IRL, doesn't have @customer in specs
+  xit 'navigates to customers#deposit_limit' do
+    within 'ul.navigation' do
+      click_link I18n.t('navigation.customer.deposit_limit')
+      expect(page).to have_current_path(deposit_limit_customer_path(customer))
+    end
+  end
+
   it 'navigates to customers#transactions' do
     within 'ul.navigation' do
       click_link I18n.t('entities.transactions')
