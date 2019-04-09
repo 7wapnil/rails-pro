a470646db784a4c4308cd0474f0d148a151b402d
Jay <jhardia@gammastack.com>
2019-04-09 11:49:13 +0300

    Remove more redundant code

--- app/graphql/payment_methods/payment_methods_query.rb
+++ app/graphql/payment_methods/payment_methods_query.rb
@@ -5,7 +5,10 @@ module PaymentMethods
     type !types[PaymentMethodType]
 
     def resolve(_obj, _args)
-      PaymentMethodsResolver.call
+      SafeCharge::Withdraw::AVAILABLE_WITHDRAW_MODES.values
+                                                    .flatten
+                                                    .compact
+                                                    .uniq
     end
   end
 end
