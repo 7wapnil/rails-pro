78c8cb2ba1030974c28e4ff9e5781b5e631012f9
Varun <vsharma@gammastack.com>
2019-04-25 10:56:05 +0300

    Integrate updates events manager into fixture change message handler

--- app/services/odds_feed/radar/fixture_change_handler.rb
+++ app/services/odds_feed/radar/fixture_change_handler.rb
@@ -13,7 +13,6 @@ module OddsFeed
 
       def handle
         return unless valid?
-        return preload_event_and_retry! unless event_exists?
 
         update_event
       end
@@ -21,16 +20,33 @@ module OddsFeed
       private
 
       def valid?
-        type_valid? && (!producer.live? || producer.live? && event_exists?)
-      end
-
-      def type_valid?
         return true if EventsManager::Entities::Event.type_match?(event_id)
 
-        log_job_failure(
-          "Event with external ID #{event_id} could not be processed yet"
-        )
+        warning = "Event with external ID #{event_id} is not supported yet"
+        log_process(:warn, warning)
+
         false
+      end
+
+      def update_event
+        msg = "Updating event ID #{event_id}, change type '#{change_type}'"
+        log_job_message(:info, msg)
+
+        update_event_producer!
+        update_event_payload!
+      end
+
+      def update_event_producer!
+        return if producer == event.producer
+
+        log_job_message(:info, "Updating producer for event ID #{event_id}")
+        event.update(producer: producer)
+      end
+
+      def update_event_payload!
+        log_job_message(:info, "Updating payload for event ID #{event_id}")
+
+        event.update!(active: false) if change_type == :cancelled
       end
 
       def event_id
@@ -41,60 +57,16 @@ module OddsFeed
         @payload['fixture_change']
       end
 
-      def producer
-        @producer ||= ::Radar::Producer.find(payload['product'])
-      end
-
-      def event_exists?
-        @event_exists ||= Event.exists?(external_id: event_id)
-      end
-
-      def preload_event_and_retry!
-        ::Radar::ScheduledEvents::EventLoadingWorker.perform_async(event_id)
-
-        log_job_failure(
-          I18n.t('errors.messages.nonexistent_event', id: event_id)
-        )
-
-        raise SilentRetryJobError,
-              I18n.t('errors.messages.nonexistent_event', id: event_id)
-      end
-
-      def update_event
-        @event = load_event
-        update_event_producer!
-        update_event_payload!
-      end
-
-      def load_event
-        log_on_update
-        EventsManager::EventLoader.call(event_id, check_existence: false)
-      end
-
-      def log_on_update
-        msg = <<-MESSAGE
-          Updating event with external ID #{event_id} \
-          on change type '#{change_type}'
-        MESSAGE
-
-        log_job_message(:info, msg.squish)
-      end
-
       def change_type
         CHANGE_TYPES[payload['change_type']]
       end
 
-      def update_event_producer!
-        return if producer == @event.producer
-
-        log_job_message(:info, "Updating producer for event ID #{event_id}")
-        @event.update(producer: producer)
+      def producer
+        @producer ||= ::Radar::Producer.find(payload['product'])
       end
 
-      def update_event_payload!
-        log_job_message(:info, "Updating payload for event ID #{event_id}")
-        @event.active = false if change_type == :cancelled
-        @event.save!
+      def event
+        @event ||= ::EventsManager::EventLoader.call(event_id)
       end
     end
   end
