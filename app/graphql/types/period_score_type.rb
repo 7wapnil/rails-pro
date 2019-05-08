09dc9cce9711c2c6fd2fc75cdfb361ea6b00ea46
Mahendra <mtyagi@gammastack.com>
2019-05-08 09:42:45 +0300

    Remove GraphQL PeriodScoreType

--- app/graphql/types/period_score_type.rb
+++
@@ -1,8 +0,0 @@
-Types::PeriodScoreType = GraphQL::ObjectType.define do
-  name 'PeriodScore'
-
-  field :id, !types.ID
-  field :score, !types.String
-  field :status_code, !types.Int
-  field :status, !types.String
-end
