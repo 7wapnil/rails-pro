5ab21337c0df90bbd976ac13978f4ec2316bdc96
Dinesh <dinesh@gammastack.com>
2019-05-15 10:30:35 +0300

    Add extra fields to bonus query

--- app/graphql/customer_bonuses/customer_bonus_type.rb
+++ app/graphql/customer_bonuses/customer_bonus_type.rb
@@ -9,6 +9,9 @@ module CustomerBonuses
     field :rollover_balance, !types.Float
     field :rollover_initial_value, !types.Float
     field :status, !types.String
+    field :max_rollover_per_bet, !types.String
+    field :min_odds_per_bet, !types.String
+    field :valid_for_days, !types.String
     field :expires_at, !types.String do
       resolve ->(obj, _args, _ctx) { obj.expires_at.strftime('%e.%m.%y') }
     end
