7447d68eb4f1e87c6f198de661d2fc6e8e948276
Mahendra <mtyagi@gammastack.com>
2019-05-21 13:58:41 +0300

    Camelize RegisterInput GraphQL type

--- app/graphql/account/register_input.rb
+++ app/graphql/account/register_input.rb
@@ -4,19 +4,19 @@ module Account
 
     argument :username, !types.String
     argument :email, !types.String
-    argument :first_name, !types.String
-    argument :last_name, !types.String
-    argument :date_of_birth, !types.String
+    argument :firstName, !types.String
+    argument :lastName, !types.String
+    argument :dateOfBirth, !types.String
     argument :password, !types.String
-    argument :password_confirmation, !types.String
+    argument :passwordConfirmation, !types.String
     argument :country, !types.String
     argument :city, !types.String
-    argument :street_address, !types.String
+    argument :streetAddress, !types.String
     argument :state, !types.String
-    argument :zip_code, !types.String
+    argument :zipCode, !types.String
     argument :gender, !types.String
     argument :phone, !types.String
-    argument :agreed_with_promotional, !types.Boolean
-    argument :b_tag, types.String
+    argument :agreedWithPromotional, !types.Boolean
+    argument :bTag, types.String
   end
 end
