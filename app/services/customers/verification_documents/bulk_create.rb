031a8b805aaa3a1f0d3a25bee1beb8bff78b7816
Varun <vsharma@gammastack.com>
2019-05-21 23:49:17 +0300

    Status to constant, remove method

--- app/services/customers/verification_documents/bulk_create.rb
+++ app/services/customers/verification_documents/bulk_create.rb
@@ -24,19 +24,18 @@ module Customers
       end
 
       def attach_document(kind, file)
-        doc = document(file)
+        doc = Documents::CreateForm.new(document: file)
 
         doc.valid? ? save_document!(kind, file) : add_error(kind, doc)
       end
 
-      def document(file)
-        Documents::CreateForm.new(document: file)
-      end
-
       def save_document!(kind, file)
-        verification_document = customer.verification_documents
-                                        .create(kind: kind,
-                                                status: :pending)
+        verification_document = customer
+                                .verification_documents
+                                .create(
+                                  kind: kind,
+                                  status: VerificationDocument::PENDING
+                                )
 
         verification_document.document.attach(file)
       end
