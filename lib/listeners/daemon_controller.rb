ba2e99820acc0b4249aff9c1da329a9e826b5f5a
Varun <vsharma@gammastack.com>
2019-06-17 16:15:42 +0300

    Fix for mkdir problem

--- lib/listeners/daemon_controller.rb
+++ lib/listeners/daemon_controller.rb
@@ -3,5 +3,4 @@
 require 'daemons'
 require File.expand_path('../../config/environment.rb', __dir__)
 
-Daemons.run(File.join(Rails.root, 'lib', 'listeners', 'daemon.rb'),
-            dir_mode: :system)
+Daemons.run(File.join(Rails.root, 'lib', 'listeners', 'daemon.rb'))
