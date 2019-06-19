5169394ecd16e7a131a8e7a02ada682dd684afbd
Mahendra <mtyagi@gammastack.com>
2019-06-19 15:59:14 +0300

    Add Customers::Summaries::Updater service

---
+++ app/services/customers/summaries/updater.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Customers
+  module Summaries
+    class Updater < ApplicationService
+      INCREMENT_ATTRIBUTES = %i[bonus_wager_amount
+                                real_money_wager_amount
+                                bonus_payout_amount
+                                real_money_payout_amount
+                                bonus_deposit_amount
+                                real_money_deposit_amount
+                                withdraw_amount
+                                signups_count].freeze
+      APPEND_ATTRIBUTES = [:betting_customer_ids].freeze
+
+      def initialize(day, attributes)
+        @attributes = attributes
+        @summary = Customers::Summary.find_or_create_by day: day
+      end
+
+      def call
+        attributes.each_pair do |key, value|
+          update_summary_attribute(key, value)
+        end
+      end
+
+      private
+
+      attr_reader :attributes, :summary
+
+      def update_summary_attribute(key, value)
+        case key.to_sym
+        when *INCREMENT_ATTRIBUTES
+          summary.increment!(key, value)
+        when *APPEND_ATTRIBUTES
+          Customers::Summary
+            .where(id: summary.id)
+            .update_all(["#{key} = array_append(#{key}, ?)", value])
+        else
+          raise NotImplementedError,
+                "#{key} update not implemented for Customers::Summary"
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/customers/summaries/updater_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+DECIMAL_ATTRIBUTES = %i[bonus_wager_amount
+                        real_money_wager_amount
+                        bonus_payout_amount
+                        real_money_payout_amount
+                        bonus_deposit_amount
+                        real_money_deposit_amount
+                        withdraw_amount].freeze
+INTEGER_ATTRIBUTES = [:signups_count].freeze
+ARRAY_ATTRIBUTES = [:betting_customer_ids].freeze
+NOT_IMPLEMENTED_ATTRIBUTE = :xxx
+
+describe Customers::Summaries::Updater do
+  subject { described_class.call(summary.day, attribute => value) }
+
+  let!(:summary) { create :customers_summary }
+
+  context 'with decimal attributes' do
+    DECIMAL_ATTRIBUTES.each do |attr|
+      let(:attribute) { attr }
+      let(:value) { rand(0.1..2.0).round(2) }
+
+      it "increments #{attr} correctly" do
+        expect { subject }.to(
+          change { summary.reload.send(attribute).to_d }.by(value)
+        )
+      end
+    end
+  end
+
+  context 'with integer attributes' do
+    INTEGER_ATTRIBUTES.each do |attr|
+      let(:attribute) { attr }
+      let(:value) { rand(1..10) }
+
+      it "increments #{attr} correctly" do
+        expect { subject }.to(
+          change { summary.reload.send(attribute) }.by(value)
+        )
+      end
+    end
+  end
+
+  context 'with array attributes' do
+    ARRAY_ATTRIBUTES.each do |attr|
+      let(:attribute) { attr }
+      let(:value) { rand(0..1000) }
+      let(:old_array) { summary.send(attribute) }
+
+      it "appends #{attr} correctly" do
+        expect { subject }.to(
+          change { summary.reload.send(attribute) }.to(old_array << value)
+        )
+      end
+    end
+  end
+
+  context 'with missing attribute' do
+    let(:attribute) { NOT_IMPLEMENTED_ATTRIBUTE }
+    let(:value) { nil }
+
+    it 'raises NotImplementedError' do
+      expect { subject }.to raise_error(NotImplementedError)
+    end
+  end
+end
