43d22f1ac37603197077c4d09e4e638b175b16b0
Varun <vsharma@gammastack.com>
2019-06-19 20:06:06 +0300

    Unacknowledge messages fix

--- lib/listeners/ticket_acceptance_listener.rb
+++ lib/listeners/ticket_acceptance_listener.rb
@@ -6,16 +6,23 @@ module Listeners
 
     BATCH_SIZE = 10
 
-    def listen
+    def listen # rubocop:disable Metrics/MethodLength
       ch = Mts::Session.instance.opened_connection.create_channel
       ch.prefetch(BATCH_SIZE)
+
       queue = ch.queue(ENV['MTS_MQ_QUEUE_CONFIRM'], durable: true)
+      consumer = TicketAcceptanceConsumer
+                 .new(ch,
+                      queue,
+                      ENV['MTS_MQ_USER'] + '.confirm-consumer',
+                      false,
+                      true)
 
-      consumer = TicketAcceptanceConsumer.new(ch, queue, '', true, true)
-
-      consumer.on_delivery do |_delivery_info, _metadata, payload|
+      consumer.on_delivery do |delivery_info, _metadata, payload|
         Rails.logger.debug payload
         ::Mts::ValidationResponseWorker.perform_async(payload)
+
+        ch.ack(delivery_info.delivery_tag)
       end
 
       queue.subscribe_with(consumer)
--- lib/listeners/ticket_cancellation_listener.rb
+++ lib/listeners/ticket_cancellation_listener.rb
@@ -6,16 +6,24 @@ module Listeners
 
     BATCH_SIZE = 10
 
-    def listen
+    def listen # rubocop:disable Metrics/MethodLength
       ch = Mts::Session.instance.opened_connection.create_channel
       ch.prefetch(BATCH_SIZE)
+
       queue = ch.queue(ENV['MTS_MQ_QUEUE_REPLY'], durable: true)
 
-      consumer = TicketCancellationConsumer.new(ch, queue, '', true, true)
+      consumer = TicketCancellationConsumer
+                 .new(ch,
+                      queue,
+                      ENV['MTS_MQ_USER'] + '.reply-consumer',
+                      false,
+                      true)
 
-      consumer.on_delivery do |_delivery_info, _metadata, payload|
+      consumer.on_delivery do |delivery_info, _metadata, payload|
         Rails.logger.debug payload
         ::Mts::CancellationResponseWorker.perform_async(payload)
+
+        ch.ack(delivery_info.delivery_tag)
       end
 
       queue.subscribe_with(consumer)
