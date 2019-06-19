02a6d4afac5610e3dfe14130a387343051ea12c0
Varun <vsharma@gammastack.com>
2019-06-19 16:52:56 +0300

    Prefetch messages from queue, automatic ack!

--- lib/listeners/ticket_acceptance_listener.rb
+++ lib/listeners/ticket_acceptance_listener.rb
@@ -4,16 +4,18 @@ module Listeners
   class TicketAcceptanceListener
     include Singleton
 
+    BATCH_SIZE = 10
+
     def listen
       ch = Mts::Session.instance.opened_connection.create_channel
+      ch.prefetch(BATCH_SIZE)
       queue = ch.queue(ENV['MTS_MQ_QUEUE_CONFIRM'], durable: true)
 
-      consumer = TicketAcceptanceConsumer.new(ch, queue, '', false, true)
+      consumer = TicketAcceptanceConsumer.new(ch, queue, '', true, true)
 
-      consumer.on_delivery do |delivery_info, _metadata, payload|
+      consumer.on_delivery do |_delivery_info, _metadata, payload|
         Rails.logger.debug payload
         ::Mts::ValidationResponseWorker.perform_async(payload)
-        ch.acknowledge(delivery_info.delivery_tag, false)
       end
 
       queue.subscribe_with(consumer)
--- lib/listeners/ticket_cancellation_listener.rb
+++ lib/listeners/ticket_cancellation_listener.rb
@@ -4,11 +4,14 @@ module Listeners
   class TicketCancellationListener
     include Singleton
 
+    BATCH_SIZE = 10
+
     def listen
       ch = Mts::Session.instance.opened_connection.create_channel
+      ch.prefetch(BATCH_SIZE)
       queue = ch.queue(ENV['MTS_MQ_QUEUE_REPLY'], durable: true)
 
-      consumer = TicketCancellationConsumer.new(ch, queue, '', false, true)
+      consumer = TicketCancellationConsumer.new(ch, queue, '', true, true)
 
       consumer.on_delivery do |_delivery_info, _metadata, payload|
         Rails.logger.debug payload
