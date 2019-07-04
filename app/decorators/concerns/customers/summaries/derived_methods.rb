1f3da1f58ff0d28c44697f6a08670be50cbe25a7
Mahendra <mtyagi@gammastack.com>
2019-07-04 09:51:28 +0300

    Fix derived methods precision

--- app/decorators/concerns/customers/summaries/derived_methods.rb
+++ app/decorators/concerns/customers/summaries/derived_methods.rb
@@ -3,12 +3,18 @@
 module Customers
   module Summaries
     module DerivedMethods
+      MONEY_PRECISION = 2
+      PNL_PRECISION = 2
       def total_wager_amount
-        bonus_wager_amount.to_f + real_money_wager_amount.to_f
+        (
+          bonus_wager_amount.to_f + real_money_wager_amount.to_f
+        ).truncate(MONEY_PRECISION).to_d
       end
 
       def total_payout_amount
-        bonus_payout_amount.to_f + real_money_payout_amount.to_f
+        (
+          bonus_payout_amount.to_f + real_money_payout_amount.to_f
+        ).truncate(MONEY_PRECISION).to_d
       end
 
       def active_customers_count
@@ -20,18 +26,27 @@ module Customers
       end
 
       def bonus_pnl_percentage
-        100.0 * (bonus_wager_amount.to_f - bonus_payout_amount.to_f) /
+        (
+          100.0 *
+          (bonus_wager_amount.to_f - bonus_payout_amount.to_f) /
           bonus_wager_amount.to_f
+        ).truncate(PNL_PRECISION).to_d
       end
 
       def real_money_pnl_percentage
-        100.0 * (real_money_wager_amount.to_f - real_money_payout_amount.to_f) /
+        (
+          100.0 *
+          (real_money_wager_amount.to_f - real_money_payout_amount.to_f) /
           real_money_wager_amount.to_f
+        ).truncate(PNL_PRECISION).to_d
       end
 
       def total_pnl_percentage
-        100.0 * (total_wager_amount.to_f - total_payout_amount.to_f) /
+        (
+          100.0 *
+          (total_wager_amount.to_f - total_payout_amount.to_f) /
           total_wager_amount.to_f
+        ).truncate(PNL_PRECISION).to_d
       end
     end
   end
