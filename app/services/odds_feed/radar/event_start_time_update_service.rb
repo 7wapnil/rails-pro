b3d354d86f903df3315ff7ebb2a9342e68f4c74f
Varun <vsharma@gammastack.com>
2019-08-02 14:59:42 +0300

    Improve logging of fixture change start time comparison

--- app/services/odds_feed/radar/event_start_time_update_service.rb
+++ app/services/odds_feed/radar/event_start_time_update_service.rb
@@ -3,13 +3,19 @@
 module OddsFeed
   module Radar
     class EventStartTimeUpdateService < ApplicationService
+      include JobLogger
+
+      TIMESTAMP_SIZE = 10
+
       def initialize(event:, payload_time:)
         @event = event
         @payload_time = payload_time
       end
 
       def call
+        # TODO: Remove API call if error doesn't raise at all
         compare_time
+
         event.update!(start_at: api_start_time)
       end
 
@@ -20,10 +26,9 @@ module OddsFeed
       def compare_time
         return if api_start_time.to_datetime == fixture_start_time
 
-        Rails.logger.warn(
-          message: "API: #{api_start_time} != Fixture: #{fixture_start_time}",
-          event_id: event.id
-        )
+        log_job_message(:warn, message: 'Start time mismatch',
+                               api_start_time: api_start_time,
+                               fixture_start_time: fixture_start_time)
       end
 
       def api_start_time
@@ -38,7 +43,7 @@ module OddsFeed
         return unless payload_time
 
         @fixture_start_time ||=
-          DateTime.strptime(payload_time.first(10), '%s')
+          DateTime.strptime(payload_time.first(TIMESTAMP_SIZE), '%s')
       end
     end
   end
