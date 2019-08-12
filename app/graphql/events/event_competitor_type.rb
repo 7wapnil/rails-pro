1f6653f62db3f125c1f9518c7153a18d256fc507
Varun <vsharma@gammastack.com>
2019-08-12 09:47:52 +0300

    [1638] Fix freezing of Cricket tournament page

--- app/graphql/events/event_competitor_type.rb
+++ app/graphql/events/event_competitor_type.rb
@@ -2,7 +2,10 @@ module Events
   EventCompetitorType = GraphQL::ObjectType.define do
     name 'EventCompetitor'
 
-    field :id, !types.ID, property: :external_id
+    field :id, !types.ID do
+      resolve ->(obj, *) { "#{obj.event_id}-#{obj.competitor_id}" }
+    end
+
     field :name, !types.String
     field :qualifier, !types.String
   end
