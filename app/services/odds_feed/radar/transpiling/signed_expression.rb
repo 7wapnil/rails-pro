017c930c861e59e1c7dfbe6648a7db8ec0e90859
Rahul <rmishra@gammastack.com>
2019-08-13 11:30:49 +0300

    1619 - Requested changes

--- app/services/odds_feed/radar/transpiling/signed_expression.rb
+++ app/services/odds_feed/radar/transpiling/signed_expression.rb
@@ -3,11 +3,12 @@ module OddsFeed
     module Transpiling
       class SignedExpression < BaseExpression
         def value(token)
-          sign = token[0]
           number = specifier(token[1..-1])
+          token_sign = token[0]
+          number_sign = number[0]
 
-          return "#{sign}#{number}" unless number[0] == '-' || number[0] == '+'
-          return "-#{number[1..-1]}" if sign != number[0]
+          return "#{token_sign}#{number}" unless %w[+ -].member?(number_sign)
+          return "-#{number[1..-1]}" if token_sign != number_sign
 
           "+#{number[1..-1]}"
         end
