38b6278a003e9290ebf3d80ae7933a8bd2b6d8c6
Mahendra <mtyagi@gammastack.com>
2019-08-30 15:40:57 +0300

    Enable sendgrid emails (#1148)

--- app/mailers/arcanebet_mailer.rb
+++ app/mailers/arcanebet_mailer.rb
@@ -2,46 +2,66 @@ class ArcanebetMailer < ApplicationMailer
   default from: 'noreply@arcanebet.com',
           subject: 'ArcaneBet'
 
+  TEMPLATES = {
+    suspicious_login:          '13ea825c-23dd-4d82-bc55-fc037d2e1a49',
+    account_verification_mail: '2f09ad2c-5db2-40ee-8c62-d2781fa7bea8',
+    email_verification_mail:   '0114629f-0d3d-4bcb-8f45-8377e4659be4',
+    reset_password_mail:       '6ce802da-57e3-4ef0-9a0a-141f840864ac'
+  }.freeze
+
   def suspicious_login(login)
-    @person = find_person(login)
+    customer = find_person(login)
+    domain = ENV['FRONTEND_URL']
+    change_password_url = "#{domain}?changePassword=1"
 
-    warn_suspicious_login(login) unless @person
+    warn_suspicious_login(login) unless customer
 
-    mail(
-      to:      @person.email,
-      subject: I18n.t('mailers.arcanebet_mailer.suspicious_login.subject')
+    smtpapi_mail(
+      TEMPLATES[__method__],
+      customer.email,
+      'changePasswordUrl': change_password_url
     )
   end
 
   def account_verification_mail
-    @person = params[:customer]
-    mail(
-      to:      @person.email,
-      subject: I18n.t('mailers.arcanebet_mailer.verification_mail.subject')
+    customer = params[:customer]
+    domain = ENV['FRONTEND_URL']
+    deposit_url = "#{domain}?depositState=1"
+
+    smtpapi_mail(
+      TEMPLATES[__method__],
+      customer.email,
+      'fullName':   customer.full_name,
+      'depositUrl': deposit_url
     )
   end
 
   def email_verification_mail
+    customer = params[:customer]
     domain = ENV['FRONTEND_URL']
-    @customer = params[:customer]
-    @verification_url =
-      "#{domain}/email_verification/#{@customer.email_verification_token}"
+    verification_url =
+      "#{domain}/email_verification/#{customer.email_verification_token}"
+    bonus_rules_url = "#{domain}/promotions/bonus-rules"
 
-    mail(
-      to:      @customer.email,
-      subject: I18n.t('mailers.arcanebet_mailer.email_verification.subject')
+    smtpapi_mail(
+      TEMPLATES[__method__],
+      customer.email,
+      'fullName':        customer.full_name,
+      'verificationUrl': verification_url,
+      'bonusRulesUrl':   bonus_rules_url
     )
   end
 
   def reset_password_mail(raw_token)
     domain = ENV['FRONTEND_URL']
-    @person = params[:customer]
-    @reset_password_url =
-      "#{domain}/reset_password/#{raw_token}"
+    customer = params[:customer]
+    reset_password_url = "#{domain}/reset_password/#{raw_token}"
 
-    mail(
-      to: @person.email,
-      subject: I18n.t('mailers.arcanebet_mailer.reset_password_mail.subject')
+    smtpapi_mail(
+      TEMPLATES[__method__],
+      customer.email,
+      'fullName':         customer.full_name,
+      'resetPasswordUrl': reset_password_url
     )
   end
 
@@ -56,5 +76,42 @@ class ArcanebetMailer < ApplicationMailer
     Rails.logger.warn(
       "Try to send suspicious login email to unpersisted person: `#{login}`."
     )
+  end
+
+  def smtpapi_headers(template_id, substitutions = nil)
+    headers(
+      'X-SMTPAPI' => template_headers(template_id)
+                       .merge(substitutions_headers(substitutions))
+                       .to_json
+    )
+  end
+
+  def smtpapi_mail(template_id, email, substitutions = nil)
+    smtpapi_headers(template_id, substitutions)
+    mail(to: email, content_type: 'text/html', body: '')
+  end
+
+  def template_headers(template_id)
+    {
+      filters: {
+        templates: {
+          settings: {
+            enable: 1,
+            template_id: template_id
+          }
+        }
+      }
+    }
+  end
+
+  def substitutions_headers(substitutions = nil)
+    return {} unless substitutions
+
+    hdrs = {}
+    substitutions.each_pair do |key, value|
+      hdrs["%#{key}%"] = [value]
+    end
+
+    { sub: hdrs }
   end
 end
