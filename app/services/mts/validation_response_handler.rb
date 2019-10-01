e686f9bd7c23ae7ac4c8a18c448f87fce803efe1
Jay <jhardia@gammastack.com>
2019-10-01 07:19:42 +0300

    1848 - Cover Mts::ValidationResponseHandler with transaction (#1253)
    * 1848 - Cover Mts::ValidationResponseHandler with transaction
    
    * 1848 - Update to develop

--- app/services/mts/validation_response_handler.rb
+++ app/services/mts/validation_response_handler.rb
@@ -14,10 +14,8 @@ module Mts
       response.bets.each do |bet|
         next if cancelled_statuses.include?(bet.status)
 
-        bet.finish_external_validation_with_acceptance! if response.accepted?
-        reject_bet!(bet) if response.rejected?
-
-        WebSocket::Client.instance.trigger_bet_update(bet)
+        finish_external_validation_with_acceptance(bet) if response.accepted?
+        finish_external_validation_with_rejection(bet) if response.rejected?
       end
     end
 
@@ -27,22 +25,35 @@ module Mts
       ::StateMachines::BetStateMachine::CANCELLED_STATUSES_MASK
     end
 
-    def reject_bet!(bet)
-      refund = EntryRequests::Factories::Refund.call(
+    def finish_external_validation_with_acceptance(bet)
+      bet.finish_external_validation_with_acceptance!
+      WebSocket::Client.instance.trigger_bet_update(bet)
+    end
+
+    def finish_external_validation_with_rejection(bet)
+      EntryRequests::BetRefundWorker.perform_async(refund_payload(bet))
+      log_bet_failed_external_validation(bet)
+    end
+
+    def refund_payload(bet)
+      {
+        id: refund_entry_request(bet).id,
+        message: response.rejection_message,
+        code: response.rejection_key
+      }
+    end
+
+    def refund_entry_request(bet)
+      EntryRequests::Factories::Refund.call(
         entry: bet.entry,
         comment: 'Bet failed external validation.'
       )
+    end
 
-      EntryRequests::RefundWorker.perform_async(refund.id)
-
+    def log_bet_failed_external_validation(bet)
       log_job_message(:info, message: 'Bet failed external validation.',
                              payload: response.rejection_json,
                              bet_id: bet.id)
-
-      bet.finish_external_validation_with_rejection!(
-        response.rejection_message,
-        code: response.rejection_key
-      )
     end
   end
 end
