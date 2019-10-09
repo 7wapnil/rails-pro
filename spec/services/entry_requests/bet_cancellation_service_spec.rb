7a864655371214f70ab75de6ca1cba21ba7d9f4d
Rahul <rmishra@gammastack.com>
2019-10-09 17:42:26 +0300

    1908 - Requested changes

--- spec/services/entry_requests/bet_cancellation_service_spec.rb
+++ spec/services/entry_requests/bet_cancellation_service_spec.rb
@@ -96,7 +96,7 @@ describe EntryRequests::BetCancellationService do
 
     before { subject }
 
-    it 'changes status to pending mts cancellation' do
+    it 'change status to pending mts cancellation' do
       expect(bet.reload).to be_pending_mts_cancellation
     end
 
