c38f75fc696c84fa9e1dca7070cd4df5efa0aea2
Mahendra <mtyagi@gammastack.com>
2019-10-23 02:47:05 +0300

    Add logging for GraphQL params parsing error

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -7,6 +7,17 @@ class GraphqlController < ApiController
 
   respond_to :json
 
+  LOGIN_REGEXP    = /"login":"([^"]*)"/
+  PASSWORD_REGEXP = /"password":"([^"]*)"/
+
+  def process_action(*args)
+    super
+  rescue ActionDispatch::Http::Parameters::ParseError => e
+    log_params_parsing_error(e)
+    render json: { message: I18n.t('graphql.internal_server_error') },
+           status: :internal_server_error
+  end
+
   def execute
     result = ArcanebetSchema.execute(
       params[:query],
@@ -46,5 +57,22 @@ class GraphqlController < ApiController
       impersonated_by: impersonated_by,
       request: request
     }
+  end
+
+  def log_params_parsing_error(error_object)
+    ip            = request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
+    user_agent    = request.headers['User-Agent']
+    raw_post_data = request.env['RAW_POST_DATA']
+    login         = raw_post_data.match(LOGIN_REGEXP)&.[](1)
+    password      = raw_post_data.match(PASSWORD_REGEXP)&.[](1)
+
+    Rails.logger.error(
+      ip: ip,
+      user_agent: user_agent,
+      raw_post_data: raw_post_data,
+      login: login,
+      password: password,
+      error_object: error_object
+    )
   end
 end
