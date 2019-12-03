5890628d2352b5945754acf37c8f001f67fdfdea
Mahendra <mtyagi@gammastack.com>
2019-12-03 15:52:47 +0300

    Add cancelled bonus logging

--- app/services/balance_calculations/every_matrix/debit_calculations.rb
+++ app/services/balance_calculations/every_matrix/debit_calculations.rb
@@ -21,6 +21,8 @@ module BalanceCalculations
       def calculated_bonus_amount
         return bonus_amount if bonus?
 
+        log_cancelled_bonus if bonus_amount.positive?
+
         0
       end
 
@@ -31,7 +33,18 @@ module BalanceCalculations
       end
 
       def bonus_amount
-        transaction.amount - calculated_real_money_amount
+        @bonus_amount ||=
+          transaction.amount - calculated_real_money_amount
+      end
+
+      def log_cancelled_bonus
+        Rails.logger.info(
+          message: 'EveryMatrix Wallet API cancelled bonus',
+          bonus_amount: bonus_amount,
+          transaction: transaction.attributes,
+          wager: wager.attributes,
+          customer_bonus: customer_bonus.attributes
+        )
       end
     end
   end
