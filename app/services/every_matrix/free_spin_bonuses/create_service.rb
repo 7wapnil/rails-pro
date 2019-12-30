0966c1d22c5ad7440745ac1a3c2315b7b11d9ecb
Mahendra <mtyagi@gammastack.com>
2019-12-30 09:43:12 +0300

    Skip missing user CSV file on Free Spin Bonus create

--- app/services/every_matrix/free_spin_bonuses/create_service.rb
+++ app/services/every_matrix/free_spin_bonuses/create_service.rb
@@ -40,6 +40,8 @@ module EveryMatrix
       def create_free_spin_bonus_wallets!
         @free_spin_bonus_wallet_ids = []
 
+        return unless customers_csv
+
         CSV.foreach(customers_csv.path, headers: true) do |row|
           customer_id = row['Id']
           wallet_type = row['Wallet'] == 'crypto' ? :crypto : :fiat
