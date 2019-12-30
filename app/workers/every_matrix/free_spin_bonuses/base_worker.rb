b6339a52385fc4a286092c7b58b7332c2fc7c647
Mahendra <mtyagi@gammastack.com>
2019-12-30 09:09:04 +0300

    Brush up EveryMatrix::FreeSpinBonuses::BaseWorker

--- app/workers/every_matrix/free_spin_bonuses/base_worker.rb
+++ app/workers/every_matrix/free_spin_bonuses/base_worker.rb
@@ -6,8 +6,7 @@ module EveryMatrix
       sidekiq_options queue: :every_matrix_free_spin_bonuses
 
       def perform(free_spin_bonus_wallet_id)
-        free_spin_bonus_wallet =
-          EveryMatrix::FreeSpinBonusWallet.find(free_spin_bonus_wallet_id)
+        @free_spin_bonus_wallet_id = free_spin_bonus_wallet_id
 
         handler_class.call(
           free_spin_bonus_wallet: free_spin_bonus_wallet
@@ -16,8 +15,14 @@ module EveryMatrix
 
       protected
 
+      attr_reader :free_spin_bonus_wallet_id
+
       def handler_class
         raise NotImplementedError, 'Implement #handler_class method'
+      end
+
+      def free_spin_bonus_wallet
+        EveryMatrix::FreeSpinBonusWallet.find(free_spin_bonus_wallet_id)
       end
     end
   end
