4beeee1b9da183a3ee4e3edd868a863dbc61e8fe
Jay <jhardia@gammastack.com>
2020-01-16 12:28:41 +0200

    [BJ-201] - Fix total wager on dashboard (#1505)

--- app/decorators/concerns/customers/summaries/derived_methods.rb
+++ app/decorators/concerns/customers/summaries/derived_methods.rb
@@ -3,48 +3,33 @@
 module Customers
   module Summaries
     module DerivedMethods
-      MONEY_PRECISION = 2
       PNL_PRECISION = 2
       def total_bet_wager_amount
-        (
-          bonus_wager_amount.to_f + real_money_wager_amount.to_f
-        ).truncate(MONEY_PRECISION).to_d
+        bonus_wager_amount.to_f + real_money_wager_amount.to_f
       end
 
       def total_bet_payout_amount
-        (
-          bonus_payout_amount.to_f + real_money_payout_amount.to_f
-        ).truncate(MONEY_PRECISION).to_d
+        bonus_payout_amount.to_f + real_money_payout_amount.to_f
       end
 
       def total_casino_wager_amount
-        (
-          casino_bonus_wager_amount.to_f + casino_real_money_wager_amount.to_f
-        ).truncate(MONEY_PRECISION).to_d
+        casino_bonus_wager_amount.to_f + casino_real_money_wager_amount.to_f
       end
 
       def total_casino_payout_amount
-        (
-          casino_bonus_payout_amount.to_f + casino_real_money_payout_amount.to_f
-        ).truncate(MONEY_PRECISION).to_d
+        casino_bonus_payout_amount.to_f + casino_real_money_payout_amount.to_f
       end
 
       def sports_ggr_amount
-        (
-          total_bet_wager_amount - total_bet_payout_amount
-        ).truncate(MONEY_PRECISION).to_d
+        total_bet_wager_amount - total_bet_payout_amount
       end
 
       def ggr_casino_amount
-        (
-          total_casino_wager_amount - total_casino_payout_amount
-        ).truncate(MONEY_PRECISION).to_d
+        total_casino_wager_amount - total_casino_payout_amount
       end
 
       def ggr_casino_bonus_amount
-        (
-          casino_bonus_wager_amount - casino_bonus_payout_amount
-        ).truncate(MONEY_PRECISION).to_d
+        casino_bonus_wager_amount - casino_bonus_payout_amount
       end
 
       def sports_ratio_by_ggr
@@ -60,10 +45,8 @@ module Customers
       end
 
       def total_ggr_amount
-        total_wager = total_bet_wager_amount + total_casino_wager_amount
-        total_payout = total_bet_payout_amount + total_casino_payout_amount
-
-        (total_wager - total_payout).truncate(MONEY_PRECISION).to_d
+        (total_bet_wager_amount + total_casino_wager_amount) -
+          (total_bet_payout_amount + total_casino_payout_amount)
       end
 
       def active_customers_count
