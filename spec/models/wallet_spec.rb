4acdcdbde38613ecaae1db4b213e7b8c5a11d0be
Mahendra <mtyagi@gammastack.com>
2020-02-13 10:52:37 +0300

    Add Wallet#initial_customer_bonus

--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -6,6 +6,9 @@ describe Wallet do
   it { is_expected.to belong_to(:customer) }
   it { is_expected.to belong_to(:currency) }
   it { is_expected.to have_many(:entries) }
+  it { is_expected.to have_many(:customer_bonuses) }
+  it { is_expected.to have_one(:customer_bonus) }
+  it { is_expected.to have_one(:initial_customer_bonus) }
 
   it { is_expected.to delegate_method(:name).to(:currency).with_prefix }
   it { is_expected.to delegate_method(:code).to(:currency).with_prefix }
@@ -35,6 +38,30 @@ describe Wallet do
     it 'emits websocket event on amount update only' do
       expect(WebSocket::Client.instance).not_to receive(:trigger_wallet_update)
       wallet.update(updated_at: Time.now)
+    end
+  end
+
+  describe 'CustomerBonus associations' do
+    let!(:primary_currency) { create(:currency, :primary) }
+    let!(:wallet) { create(:wallet, currency: primary_currency) }
+
+    before do
+      create(:customer_bonus,
+             wallet: wallet,
+             customer: wallet.customer,
+             status: 'initial')
+      create(:customer_bonus,
+             wallet: wallet,
+             customer: wallet.customer,
+             status: 'expired')
+    end
+
+    it 'returns expired bonus as #customer_bonus' do
+      expect(wallet.customer_bonus.status).to eq('expired')
+    end
+
+    it 'returns initial bonus as #initial_customer_bonus' do
+      expect(wallet.initial_customer_bonus.status).to eq('initial')
     end
   end
 end
