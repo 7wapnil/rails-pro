fde3f6e5e290f9cb8698472e35ccc22744caeadd
Mahendra <mtyagi@gammastack.com>
2020-02-19 17:14:33 +0200

    Tests for locking customer

---
+++ spec/features/customers_update_lock.rb
@@ -0,0 +1,49 @@
+describe Customer, '#update_lock' do
+  let(:per_page_count) { 10 }
+
+  context 'signed in' do
+    let(:customer) { create(:customer) }
+
+    before do
+      login_as create(:admin_user), scope: :user
+      visit customer_path(customer)
+    end
+
+    it 'shows account lock block' do
+      expect(page).to have_content(I18n.t('attributes.account_lock'))
+    end
+
+    it 'lock customer' do
+      within 'form#account_lock_form' do
+        select sample_lock_reason, from: :customer_lock_reason
+        find('#customer_locked').check
+
+        click_submit
+      end
+      expect(customer.reload.locked).to be(true)
+    end
+
+    def sample_lock_reason
+      I18n.t("lock_reasons.#{Customer.lock_reasons.values.sample}")
+    end
+  end
+
+  context 'when unlock customer' do
+    let(:customer) { create(:customer, :locked) }
+
+    before do
+      login_as create(:admin_user), scope: :user
+      visit customer_path(customer)
+    end
+
+    it 'unlock customer' do
+      within 'form#account_lock_form' do
+        select '', from: :customer_lock_reason
+        find('#customer_locked').uncheck
+
+        click_submit
+      end
+      expect(customer.reload.locked).to be(false)
+    end
+  end
+end
